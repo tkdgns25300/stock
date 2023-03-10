b1470048d32304806aa87e7ea89a9cb1f501b024
이상훈 <tkdgns25300@gmail.com>
2023-03-10 09:00:00 +0900

    stock module base structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StockController } from './stock/stock.controller';
+import { StockService } from './stock/stock.service';
+import { StockModule } from './stock/stock.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [StockModule],
+  controllers: [AppController, StockController],
+  providers: [AppService, StockService],
 })
 export class AppModule {}
