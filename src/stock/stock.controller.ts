7b782d3c6323a7cbbf8d52aa0cdf722a16ba62e8
이상훈 <tkdgns25300@gmail.com>
2023-03-15 09:00:00 +0900

    request/response API format

--- src/stock/stock.controller.ts
+++ src/stock/stock.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from "@nestjs/common";
+import { StockService } from "./stock.service";
 
-@Controller('stock')
-export class StockController {}
+@Controller("stock")
+export class StockController {
+	constructor(private stockService: StockService) {}
+
+	// 시가총액 Top 10 기업 조회
+	@Get("top-market-cap")
+	getTopMarketCap() {
+		return this.stockService.getTopMarketCap();
+	}
+}
