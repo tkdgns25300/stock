f2aa1e798387f66430228f61462a1b83da9b9536
sanghun lee <tkdgns25300@gmail.com>
2023-03-19 16:43:23 +0900

    feat: utils module생성
    utils 모듈을 생성하였습니다. 이 모듈은 client에게 API를 제공하기 위한
    모듈이 아닙니다. 서버 자체적으로 데이터 변환, API 테스트, 리소스 관리
    등의 기능을 수행하기 위한 모듈입니다.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 import { CompanyModule } from "./company/company.module";
+import { UtilsModule } from './utils/utils.module';
 
 @Module({
-  imports: [CompanyModule],
+  imports: [CompanyModule, UtilsModule],
   controllers: [],
   providers: [],
 })
