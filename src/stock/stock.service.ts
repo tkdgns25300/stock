3ce2dd9c330e552ce2ab7cfe2bd5cec7a3456704
이상훈 <tkdgns25300@gmail.com>
2023-03-22 09:00:00 +0900

    fix : 유가증권 일별매매정보 조회

--- src/stock/stock.service.ts
+++ src/stock/stock.service.ts
@@ -6,17 +6,19 @@ import axios from "axios";
 export class StockService {
 	async stkByddTrd() {
 		try {
-			const url = "http://data-dbg.krx.co.kr/svc/sample/apis/sto/stk_bydd_trd.json";
+			const url = "http://data-dbg.krx.co.kr/svc/apis/sto/stk_bydd_trd?basDd=20230414";
+
 			const response = await axios.get(url, {
+				responseType: "json", // 응답 데이터 형식을 text로 설정
 				headers: {
-					"User-Agent": "axios",
-					Authorization: `Bearer ${process.env.API_KEY}`, // API 키를 사용하여 인증합니다.
+					AUTH_KEY: process.env.API_KEY, // API 키를 AUTH_KEY 필드에 추가하여 전달합니다.
 				},
 			});
 
-			return new PageResObj(response, "유가증권 일별매매정보 조회에 성공하였습니다.");
+			return new PageResObj(response.data, "유가증권 일별매매정보 조회에 성공하였습니다.");
 		} catch (error) {
 			return new PageResObj({}, error.message, true);
 		}
 	}
 }
+//
