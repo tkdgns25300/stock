d1db70afc1c3a14d3c263cbc0deb8d7c0a4cb3a2
sanghun lee <tkdgns25300@gmail.com>
2023-03-26 18:43:23 +0900

    feat: Tailwind CSS 설치 및 설정
    CSS 라이브러리로 Tailwind CSS를 사용합니다.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import SearchComponent from "./components/SearchComponent";
 
 const App: React.FC = () => {
-  return (
-    <div>
-      <h1>My App</h1>
-      <SearchComponent />
-    </div>
-  );
+	return (
+		<div>
+			<h1>My App</h1>
+			<SearchComponent />
+		</div>
+	);
 };
 
 export default App;
