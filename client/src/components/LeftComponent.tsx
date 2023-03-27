2afdf145173ad6036843d6731ab742a6e9deaf54
sanghun lee <tkdgns25300@gmail.com>
2023-03-27 18:43:23 +0900

    feat: left component 작성
    화면을 분할하여 사이트가 동작합니다. 이를 위한 왼쪽 분할 화면 컴포넌트들
    생성하였습니다.

---
+++ client/src/components/LeftComponent.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SearchComponent from "./SearchComponent";
+
+const LeftComponent: React.FC = () => {
+	return (
+		<div className="bg-black text-white">
+			<SearchComponent />
+		</div>
+	);
+};
+
+export default LeftComponent;
