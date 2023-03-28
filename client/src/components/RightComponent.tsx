ff55bca12709ecf4b6b43a5770b5b51e3298ee50
sanghun lee <tkdgns25300@gmail.com>
2023-03-28 18:43:23 +0900

    feat: right component 생성
    분할 화면 중, 오른쪽 레이아웃을 담당하는 컴포넌트들 생성하였습니다.

---
+++ client/src/components/RightComponent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RightComponent: React.FC = () => {
+	return (
+		<div className="bg-white">
+			<h1>Welcome to my site!</h1>
+			<p>This is a site about...</p>
+		</div>
+	);
+};
+export default RightComponent;
