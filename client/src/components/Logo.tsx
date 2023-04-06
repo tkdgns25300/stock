9bb97a35e63c1494e406a0bb290c5e0d78fdbf3d
sanghun lee <tkdgns25300@gmail.com>
2023-04-06 12:43:23 +0900

    improve: 로고 이미지 추가 및 크기 수정
    사용할 로고이미지 3가지 추가 및 로고 크기를 수정하였습니다.

--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import LogoSvg from "../assets/images/logo3.svg";
+import LogoSvg from "../assets/images/logo1.svg";
 
 const Logo: React.FC = () => {
 	return (
 		<Link to="/">
-			<img src={LogoSvg} alt="Logo" className="absolute w-40 mx-6 my-8" />
+			<img src={LogoSvg} alt="Logo" className="absolute w-56 p-0 m-6" />
 		</Link>
 	);
 };
