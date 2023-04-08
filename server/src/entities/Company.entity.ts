1ed1a551a1ebd8ab27a6e2a15f6626e1d265f286
sanghun lee <tkdgns25300@gmail.com>
2023-04-08 13:43:23 +0900

    feat: 기업 정보 entity 생성
    기업 정보와 맵핑되는에 엔티티를 생성하였습니다.

---
+++ server/src/entities/Company.entity.ts
@@ -0,0 +1,97 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("company_info")
+export class CompanyInfo {
+	@PrimaryGeneratedColumn({
+		comment: "회사 아이디",
+	})
+	id: number;
+
+	@Column({
+		type: "varchar",
+		length: 100,
+		comment: "한글명",
+	})
+	name: string;
+
+	@Column({
+		type: "varchar",
+		length: 100,
+		comment: "한글명_상세",
+	})
+	detailed_name: string;
+
+	@Column({
+		type: "varchar",
+		length: 100,
+		comment: "영문명",
+	})
+	english_name: string;
+
+	@Column({
+		type: "varchar",
+		length: 100,
+		comment: "업종명",
+	})
+	industry_name: string;
+
+	@Column({
+		type: "varchar",
+		length: 10,
+		comment: "업종코드",
+	})
+	industry_code: string;
+
+	@Column({
+		type: "int",
+		comment: "자본금",
+	})
+	capital: number;
+
+	@Column({
+		type: "varchar",
+		length: 10,
+		comment: "통화구분",
+	})
+	currency: string;
+
+	@Column({
+		type: "int",
+		comment: "결산월",
+	})
+	fiscal_month: string;
+
+	@Column({
+		type: "varchar",
+		length: 50,
+		comment: "대표이사",
+	})
+	ceo: string;
+
+	@Column({
+		type: "varchar",
+		length: 20,
+		comment: "대표전화",
+	})
+	main_phone: string;
+
+	@Column({
+		type: "varchar",
+		length: 200,
+		comment: "주소",
+	})
+	address: string;
+
+	@Column({
+		type: "varchar",
+		length: 100,
+		comment: "홈페이지",
+	})
+	website: string;
+
+	@CreateDateColumn({
+		type: "date",
+		comment: "설립일",
+	})
+	founded_date: Date;
+}
