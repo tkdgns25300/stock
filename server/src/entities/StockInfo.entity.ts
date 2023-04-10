7231280c4641f1c7b10c68e247da8dfd68e5667d
sanghun lee <tkdgns25300@gmail.com>
2023-04-10 15:43:23 +0900

    feat: 주식 정보 엔티티 생성
    주식 정보 테이블과 매핑되는 엔티티를 생성하였습니다.

---
+++ server/src/entities/StockInfo.entity.ts
@@ -0,0 +1,66 @@
+import { Entity, PrimaryColumn, Column, CreateDateColumn } from "typeorm";
+
+@Entity("stock_info")
+export class StockInfo {
+	@PrimaryColumn({
+		type: "varchar",
+		length: 15,
+		comment: "표준코드",
+	})
+	standard_code: string;
+
+	@Column({
+		type: "varchar",
+		length: 10,
+		comment: "종목코드",
+	})
+	stock_code: string;
+
+	@CreateDateColumn({
+		type: "date",
+		comment: "상장일",
+	})
+	listing_date: Date;
+
+	@Column({
+		type: "int",
+		nullable: true,
+		comment: "액면가",
+	})
+	face_value: number | null;
+
+	@Column({
+		type: "bigint",
+		comment: "상장주식수",
+	})
+	listed_shares: number;
+
+	@Column({
+		type: "varchar",
+		length: 20,
+		comment: "시장구분",
+	})
+	market_type: string;
+
+	@Column({
+		type: "varchar",
+		length: 20,
+		comment: "주식종류",
+	})
+	stock_type: string;
+
+	@Column({
+		type: "varchar",
+		length: 50,
+		nullable: true,
+		comment: "소속부",
+	})
+	affiliation: string | null;
+
+	@Column({
+		type: "varchar",
+		length: 10,
+		comment: "증권구분",
+	})
+	security_type: string;
+}
