a98d97ac16905181be1445d9b8e987935d048341
sanghun lee <tkdgns25300@gmail.com>
2023-04-16 16:43:23 +0900

    feat: types 관리를 위한 파일 생성
    interface등의 타입 설정 파일을 관리하기 위한 폴더 types를
    생성하였습니다.

---
+++ server/src/types/CompanyInfo.ts
@@ -0,0 +1,16 @@
+interface CompanyInfo {
+	name: string;
+	detailed_name: string;
+	english_name: string;
+	description: string;
+	industry_name: string;
+	industry_code: string;
+	capital: number;
+	currency: string;
+	fiscal_month: number;
+	ceo: string;
+	main_phone: string;
+	address: string;
+	website: string;
+	founded_date: Date;
+}
---
+++ server/src/types/CompanyInfoCSVRowData.ts
@@ -0,0 +1,17 @@
+interface CompanyInfoCSVRowData {
+	종목코드: string;
+	종목명: string;
+	시장구분: string;
+	소속부: string;
+	업종코드: string;
+	업종명: string;
+	결산월: string;
+	지정자문인: string;
+	상장주식수: string;
+	액면가: string;
+	자본금: string;
+	통화구분: string;
+	대표이사: string;
+	대표전화: string;
+	주소: string;
+}
