e2c1537f0110d043f3d6f9f57aabc04600d2280a
sanghun lee <tkdgns25300@gmail.com>
2023-04-23 10:43:23 +0900

    feat: Server To Database Connect
    App Module에 Mysql DB와 연결을 위한 설정을 해주었습니다.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from "@nestjs/common";
 import { CompanyModule } from "./company/company.module";
-import { UtilsModule } from './utils/utils.module';
+import { UtilsModule } from "./utils/utils.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [CompanyModule, UtilsModule],
-  controllers: [],
-  providers: [],
+	imports: [
+		TypeOrmModule.forRoot({
+			type: "mysql",
+			host: "localhost",
+			port: 3306,
+			username: "root",
+			password: "tkdgns125@",
+			database: "stockpedia",
+			autoLoadEntities: true,
+			synchronize: true,
+		}),
+		CompanyModule,
+		UtilsModule,
+	],
+	controllers: [],
+	providers: [],
 })
 export class AppModule {}
