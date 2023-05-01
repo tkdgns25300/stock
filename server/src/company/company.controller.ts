c336dabea4d22f3b0d3d71d65225481f119eb087
sanghun lee <tkdgns25300@gmail.com>
2023-05-01 13:43:23 +0900

    refactor: 기업 설명 스크래핑 API 삭제
    DB를 통해 Describe를 포함한 정보 조회가 가능하므로, 기업 설명스크래핑
    API를 삭제하였습니다.

--- server/src/company/company.controller.ts
+++ server/src/company/company.controller.ts
@@ -4,10 +4,4 @@ import { CompanyService } from "./company.service";
 @Controller("company")
 export class CompanyController {
 	constructor(private companyService: CompanyService) {}
-
-	// 기업 Description 조회
-	@Get("/description/:stockCode")
-	async getCompanyDescription(@Param("stockCode") stockCode: string): Promise<string> {
-		return await this.companyService.getCompanyDescription(stockCode);
-	}
 }
--- server/src/company/company.service.ts
+++ server/src/company/company.service.ts
@@ -2,26 +2,4 @@ import { Injectable } from "@nestjs/common";
 import puppeteer from "puppeteer";
 
 @Injectable()
-export class CompanyService {
-	async getCompanyDescription(stockCode: string): Promise<string> {
-		const url = `https://finance.naver.com/item/main.naver?code=${stockCode}`;
-
-		try {
-			const browser = await puppeteer.launch();
-			const page = await browser.newPage();
-			await page.goto(url);
-
-			const description = await page.evaluate(() => {
-				return document.querySelector("#summary_info").textContent.trim();
-			});
-
-			// description에서 "기업 개요"부분 삭제 및 전체적으로 글 정리 필요
-
-			await browser.close();
-			return description;
-		} catch (error) {
-			console.error("Error scraping data:", error);
-			return "Failed to retrieve data";
-		}
-	}
-}
+export class CompanyService {}
