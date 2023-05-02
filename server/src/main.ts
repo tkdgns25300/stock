4bf1c7d86b9e575128abbf97b6fdded76ec490e1
sanghun lee <tkdgns25300@gmail.com>
2023-05-02 13:43:23 +0900

    feat: 서버 시작 log 생성
    서버 생성 및 작동 시 서버의 시작과 포트번호를 알려주는 로그를
    생성하였습니다.

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,18 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { Logger } from "@nestjs/common";
 
 async function bootstrap() {
+	const logger = new Logger();
 	const app = await NestFactory.create(AppModule);
 
 	// API 및 버전 명시
 	app.setGlobalPrefix("api/v1");
 
-	await app.listen(3000);
+	// 서버 시작 log
+	const port = 3000;
+
+	await app.listen(port);
+	logger.log(`Application listening on port ${port}`);
 }
 bootstrap();
