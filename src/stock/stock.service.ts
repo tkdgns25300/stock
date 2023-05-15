1a23b14c25302066e7c6f265e77922908b41c8a8
이상훈 <tkdgns25300@gmail.com>
2023-05-15 09:00:00 +0900

    코스넥 종목기본정보 조회 API

--- src/stock/stock.service.ts
+++ src/stock/stock.service.ts
@@ -128,6 +128,7 @@ export class StockService {
 				},
 			});
 
+			return new PageResObj(response.data, "코스넥 종목기본정보 조회에 성공하였습니다.");
 			// /**
 			//  * 유저의 권한 확인
 			//  * 관리자인가 소유자인가
@@ -162,3 +163,29 @@ export class StockService {
 		}
 	}
 }
+
+//**
+//  * API 요청자가 참여자일 경우 익명 게시글의 게시자(user Email) 삭제 후 리턴
+//  * - 현재 유저의 권한먼저 확인(관리자 or 참여자)
+//  */
+// const spaceMember = await this.spaceMemberRepository.findOne({
+// 	where: {
+// 		space: { id: spaceId },
+// 		user: { id: user.id },
+// 	},
+// });
+// // 참여자가 아닐 경우
+// if (!spaceMember) {
+// 	return new PageResObj({}, "Not Participant", true);
+// }
+
+// const currentUserAuth: SpaceRoleType = spaceMember.memberRoleType;
+// if (currentUserAuth === SpaceRoleType.PARTICIPANT) {
+// 	for (const post of allPost) {
+// 		if (post.isAnonymous && post.authorEmail !== user.email) {
+// 			delete post.authorEmail;
+// 		}
+// 	}
+// }
+
+// return new PageResObj(allPost, "Get All Post Success");
