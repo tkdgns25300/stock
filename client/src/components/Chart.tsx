ef1c0571d66c3be92e7ab6025a8e44ce26d3660a
sanghun lee <tkdgns25300@gmail.com>
2023-05-29 12:43:23 +0900

    feat: rendering chart
    가져온 데이터를 토대로 차트를 렌더링 하였습니다.

--- client/src/components/Chart.tsx
+++ client/src/components/Chart.tsx
@@ -1,6 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
 
+interface ChartData {
+	stck_bsop_date: string;
+	stck_oprc: string;
+	stck_clpr: string;
+	stck_hgpr: string;
+	stck_lwpr: string;
+	acml_vol: string;
+}
+
 const Chart = () => {
 	const [chartData, setChartData] = useState([]);
 	const [loading, setLoading] = useState(true);
@@ -8,39 +17,39 @@ const Chart = () => {
 	useEffect(() => {
 		const fetchData = async () => {
 			try {
-				const headers = {
-					"Content-Type": "application/json; charset=utf-8",
-					appkey: "PSr2axang31iCePXN2s7ekVDxtKrXhTgPCs2",
-					appsecret:
-						"CHew135tcQ1GMyh4G/A4uibl5FjYI3blt7vU/0/1Sk2x1izcjI1NbdbKMOeQy0TfRyObQgOg9iEO1m71epi7fGV2N5ts2daXPENBlLQ6DfD0fw4k3eVqXdCTYDJHIt1t7Mk7JFpzfAEYdJm8NzYnrVQSCCtmgyuJ0YkWEU0XGwVsWd5TgGE=",
-					tr_id: "FHKST03010100",
-					custtype: "P",
-					Authorization:
-						"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0b2tlbiIsImF1ZCI6IjFlNjdkZjQ1LTMwZTAtNDZjNC1iNjNkLWZkMzVmN2Y5OTQ0YiIsInByZHRfY2QiOiIiLCJpc3MiOiJ1bm9ndyIsImV4cCI6MTcxNzU4MDEyMiwiaWF0IjoxNzE3NDkzNzIyLCJqdGkiOiJQU3IyYXhhbmczMWlDZVBYTjJzN2VrVkR4dEtyWGhUZ1BDczIifQ.qYZm12EKASEYYEWayqfMMUTPjNbxr3yVyPJ9bnn8oixXNFfxqQy1uLpXeltGoFLRKrqDexGOgucvV4bz0ZZvhA",
-				};
-				const query = {
-					FID_COND_MRKT_DIV_CODE: "J",
-					FID_INPUT_ISCD: "005930",
-					FID_INPUT_DATE_1: "20240501",
-					FID_INPUT_DATE_2: "20240531",
-					FID_PERIOD_DIV_CODE: "D",
-					FID_ORG_ADJ_PRC: "1",
-				};
-				const response = await fetch(
-					"https://openapi.koreainvestment.com:9443/uapi/domestic-stock/v1/quotations/inquire-daily-itemchartprice?" +
-						new URLSearchParams(query),
-					{
-						headers,
-					},
-				);
+				const response = await fetch(`${process.env.REACT_APP_SERVER_URI}/company/chart-data`);
 				const data = await response.json();
-				console.log(data);
-				setChartData(data);
+				const sortedData = data.result.sort((a: ChartData, b: ChartData) =>
+					a.stck_bsop_date.localeCompare(b.stck_bsop_date),
+				);
+
+				const formatDate = (dateStr: string) => {
+					const year = dateStr.substring(0, 4);
+					const month = dateStr.substring(4, 6);
+					const day = dateStr.substring(6, 8);
+					return `${month}/${day}/${year}`;
+				};
+
+				setChartData(
+					sortedData.map((item: ChartData, i: number) => ({
+						name: `${parseInt(item.stck_bsop_date.substring(4, 6), 10)}/${parseInt(
+							item.stck_bsop_date.substring(6, 8),
+							10,
+						)}`,
+						date: formatDate(item.stck_bsop_date),
+						open: item.stck_oprc,
+						close: item.stck_clpr,
+						high: item.stck_hgpr,
+						low: item.stck_lwpr,
+						volume: item.acml_vol,
+					})),
+				);
 				setLoading(false);
 			} catch (error) {
 				console.error(error);
 			}
 		};
+
 		fetchData();
 	}, []);
 
@@ -49,7 +58,7 @@ const Chart = () => {
 	}
 
 	return (
-		<ResponsiveContainer width="100%" height="100%">
+		<ResponsiveContainer width="95%" height={window.innerHeight / 4}>
 			<AreaChart
 				width={500}
 				height={400}
@@ -61,11 +70,22 @@ const Chart = () => {
 					bottom: 0,
 				}}
 			>
-				<CartesianGrid strokeDasharray="3 3" />
-				<XAxis dataKey="name" />
-				<YAxis />
+				<defs>
+					<linearGradient id="color" x1="0" y1="0" x2="0" y2="1">
+						<stop offset="5%" stopColor="#FF0000" stopOpacity={0.8} />
+						<stop offset="95%" stopColor="#FF0000" stopOpacity={0} />
+					</linearGradient>
+				</defs>
+				<CartesianGrid strokeDasharray="3 3" />/
+				<XAxis dataKey="name" tickLine={false} axisLine={false} />
+				<YAxis dataKey="close" orientation="right" />
 				<Tooltip />
-				<Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+				<Area type="monotone" dataKey="date" stroke="none" activeDot={false} fillOpacity={0} fill="#808080" />
+				<Area type="monotone" dataKey="open" stroke="none" activeDot={false} fillOpacity={0} fill="#808080" />
+				<Area type="monotone" dataKey="close" stroke="#8884d8" fillOpacity={1} fill="url(#color)" />
+				<Area type="monotone" dataKey="high" stroke="none" activeDot={false} fillOpacity={0} fill="#808080" />
+				<Area type="monotone" dataKey="low" stroke="none" activeDot={false} fillOpacity={0} fill="#808080" />
+				<Area type="monotone" dataKey="volume" stroke="none" activeDot={false} fillOpacity={0} fill="#808080" />
 			</AreaChart>
 		</ResponsiveContainer>
 	);
