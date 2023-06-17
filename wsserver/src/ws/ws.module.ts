4d869bf2495680f8c171cac8419543e04c175201
sanghun lee <tkdgns25300@gmail.com>
2023-06-17 13:43:23 +0900

    feat: (wsserver) ws service 생성
    웹소켓 관련 로직을 처리하는 데 필요한 service를 생성하였습니다.

--- wsserver/src/ws/ws.module.ts
+++ wsserver/src/ws/ws.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { WsController } from './ws.controller';
+import { WsService } from './ws.service';
 
 @Module({
-  controllers: [WsController]
+  controllers: [WsController],
+  providers: [WsService],
 })
 export class WsModule {}
