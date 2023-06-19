2bdf3fdf7839d1bedd4efc7324e7dac2c6cb2afb
sanghun lee <tkdgns25300@gmail.com>
2023-06-19 13:43:23 +0900

    feat: ws 모듈 코드 이전
    기존의 api-server에 있던 ws관련 코드들을 wsserver의 ws모듈로
    이전하였습니다.

--- wsserver/src/ws/ws.controller.ts
+++ wsserver/src/ws/ws.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, OnModuleInit } from '@nestjs/common';
+import { WsService } from './ws.service';
 
 @Controller('ws')
-export class WsController {}
+export class WsController implements OnModuleInit {
+  constructor(private wsService: WsService) {}
+
+  onModuleInit() {
+    this.wsService.connectWebSocket(); // onModuleInit에서 웹소켓 연결 초기화
+  }
+
+  // WebSocket 연결
+  @Get('connect')
+  connectWebSocket() {
+    this.wsService.connectWebSocket();
+  }
+}
