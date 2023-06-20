07c0be92894a79ca62d6a6206977e25dcc6e181b
sanghun lee <tkdgns25300@gmail.com>
2023-06-20 13:43:23 +0900

    refactor: (apiserver) ws 모듈 삭제
    ws모듈을 wsserver로 이전함에 따라 apiserver에 있던 ws모듈을
    삭제하였습니다.

--- server/src/modules/ws/ws.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from "@nestjs/common";
-import { WsService } from "./ws.service";
-
-@Controller("ws")
-export class WsController {
-	constructor(private wsService: WsService) {}
-}
