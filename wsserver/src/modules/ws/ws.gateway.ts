09a2afb070910f40c329eb494a44f7d948dbc551
sanghun lee <tkdgns25300@gmail.com>
2023-06-29 13:43:23 +0900

    feat: (wsserver) 내부 websocket 설정
    KIS웹소켓으로부터 정보를 가져오고, 이 정보를 프론트와 주고받는 데 사용될
    내부 WebSocket을 설정하였습니다.

---
+++ wsserver/src/modules/ws/ws.gateway.ts
@@ -0,0 +1,46 @@
+// ws.gateway.ts
+import {
+	WebSocketGateway,
+	WebSocketServer,
+	SubscribeMessage,
+	OnGatewayConnection,
+	OnGatewayDisconnect,
+} from "@nestjs/websockets";
+import { Server, Socket } from "socket.io";
+import { ExternalWsService } from "./external-ws.service";
+import { Injectable, Logger } from "@nestjs/common";
+
+@Injectable()
+@WebSocketGateway()
+export class WsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+	@WebSocketServer() server: Server;
+	private clients: Map<string, Socket> = new Map();
+	private readonly logger = new Logger(WsGateway.name);
+
+	constructor(private externalWsService: ExternalWsService) {
+		// 외부 WebSocket 메시지를 클라이언트로 전달
+		this.externalWsService.onMessage((data) => {
+			this.broadcastToClients("messageFromExternal", data);
+		});
+	}
+
+	handleConnection(client: Socket) {
+		this.logger.log(`Client connected: ${client.id}`);
+		this.clients.set(client.id, client);
+	}
+
+	handleDisconnect(client: Socket) {
+		this.logger.log(`Client disconnected: ${client.id}`);
+		this.clients.delete(client.id);
+	}
+
+	@SubscribeMessage("messageToServer")
+	handleMessage(client: Socket, payload: any): void {
+		this.logger.log(`Received message from ${client.id}: ${payload}`);
+		this.externalWsService.sendMessage(payload); // 외부 WebSocket 서버로 메시지 전달
+	}
+
+	broadcastToClients(event: string, message: any) {
+		this.server.emit(event, message);
+	}
+}
