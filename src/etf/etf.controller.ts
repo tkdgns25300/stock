45bbb5ce80344c1a14e8a08b3707b33160e0774f
이상훈 <tkdgns25300@gmail.com>
2023-07-12 09:00:00 +0900

    ETF 일별매매정보 API

--- src/etf/etf.controller.ts
+++ src/etf/etf.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from "@nestjs/common";
+import { EtfService } from "./etf.service";
 
-@Controller('etf')
-export class EtfController {}
+@Controller("etf")
+export class EtfController {
+	constructor(private etfService: EtfService) {}
+
+	// ETF 일별매매정보
+	@Get("etf_bydd_trd")
+	getKrxDdTrd() {
+		return this.etfService.etfByddTrd();
+	}
+}
