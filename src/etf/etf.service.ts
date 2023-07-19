6f92d92483fd8f89eb9a3536f780edf453127bd2
이상훈 <tkdgns25300@gmail.com>
2023-07-19 09:00:00 +0900

    delete unusable code

--- src/etf/etf.service.ts
+++ src/etf/etf.service.ts
@@ -16,35 +16,6 @@ export class EtfService {
 			});
 
 			return new PageResObj(response.data, "ETF 일별매매정보 조회에 성공하였습니다.");
-			// /**
-			//  * 유저의 권한 확인
-			//  * 관리자인가 소유자인가
-			//  */
-			// let isOwner: boolean;
-			// let isAdmin: boolean = false;
-
-			// const spaceMember = await this.spaceMemberRepository.findOne({
-			// 	where: {
-			// 		space: { id: spaceId },
-			// 		user: { id: user.id },
-			// 	},
-			// });
-			// // 참여자가 아닐 경우
-			// if (!spaceMember) {
-			// 	return new PageResObj({}, "Not Participant", true);
-			// }
-
-			// const currentUserAuth: SpaceRoleType = spaceMember.memberRoleType;
-			// if (currentUserAuth === SpaceRoleType.ADMIN) isAdmin = true;
-			// if (post.authorEmail === user.email) isOwner = true;
-
-			// if (!isAdmin && !isOwner) {
-			// 	return new PageResObj({}, "Not Authorized: Neither Admin Nor Owner", true);
-			// }
-
-			// // 삭제(soft-delete)
-			// post.isDeleted = true;
-			// await this.postRepository.save(post);
 		} catch (error) {
 			return new PageResObj({}, error.message, true);
 		}
