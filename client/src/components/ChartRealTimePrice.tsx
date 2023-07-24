09aa117f4e90fa317dbd3a7ce9c789bf8218387d
sanghun lee <tkdgns25300@gmail.com>
2023-07-24 13:43:23 +0900

    fix: (client) websocket 연결 주석처리
    현재 서버측 문제로 websocket연결이 불가능하기 때문에 관련 로직을 주석
    처리 하였습니다.

--- client/src/components/ChartRealTimePrice.tsx
+++ client/src/components/ChartRealTimePrice.tsx
@@ -3,42 +3,42 @@ import io from "socket.io-client";
 import { ChartRealTimePriceProps } from "./types/Chart/interface";
 
 const ChartRealTimePrice: React.FC<ChartRealTimePriceProps> = ({ stockCode, currentPrice, setCurrentPrice }) => {
-	useEffect(() => {
-		const webSocket = io(`${process.env.REACT_APP_WEBSOCKET_SERVER_WS_URI}`, {
-			withCredentials: true,
-			transports: ["websocket"],
-		});
+	// useEffect(() => {
+	// 	const webSocket = io(`${process.env.REACT_APP_WEBSOCKET_SERVER_WS_URI}`, {
+	// 		withCredentials: true,
+	// 		transports: ["websocket"],
+	// 	});
 
-		webSocket.on("connect", () => {
-			console.log("Connected to WebSocket server");
-			webSocket.emit("messageToServer", { stockCode });
-		});
+	// 	webSocket.on("connect", () => {
+	// 		console.log("Connected to WebSocket server");
+	// 		webSocket.emit("messageToServer", { stockCode });
+	// 	});
 
-		webSocket.on("disconnect", () => {
-			console.log("Disconnected from WebSocket server");
-		});
+	// 	webSocket.on("disconnect", () => {
+	// 		console.log("Disconnected from WebSocket server");
+	// 	});
 
-		webSocket.on("connect_error", (error) => {
-			console.error("WebSocket connect error:", error);
-		});
+	// 	webSocket.on("connect_error", (error) => {
+	// 		console.error("WebSocket connect error:", error);
+	// 	});
 
-		webSocket.on("error", (error) => {
-			console.error("WebSocket error:", error);
-		});
+	// 	webSocket.on("error", (error) => {
+	// 		console.error("WebSocket error:", error);
+	// 	});
 
-		webSocket.on("message", (message) => {
-			console.log("WebSocket message:", message);
-			if (message.event === "messageFromKSI" && message.data.stockCode === stockCode) {
-				setCurrentPrice(message.data.price);
-			}
-		});
+	// 	webSocket.on("message", (message) => {
+	// 		console.log("WebSocket message:", message);
+	// 		if (message.event === "messageFromKSI" && message.data.stockCode === stockCode) {
+	// 			setCurrentPrice(message.data.price);
+	// 		}
+	// 	});
 
-		return () => {
-			if (webSocket.connected) {
-				webSocket.close();
-			}
-		};
-	}, [stockCode, setCurrentPrice]);
+	// 	return () => {
+	// 		if (webSocket.connected) {
+	// 			webSocket.close();
+	// 		}
+	// 	};
+	// }, [stockCode, setCurrentPrice]);
 
 	if (currentPrice === "") {
 		return <div>Loading...</div>;
