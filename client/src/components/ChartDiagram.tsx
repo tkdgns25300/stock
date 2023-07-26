1ee23be9c37f4219047eab92dcfa2f297cb24411
sanghun lee <tkdgns25300@gmail.com>
2023-07-26 13:43:23 +0900

    improve: (client) ChartDiagram 컴포넌트 디자인

--- client/src/components/ChartDiagram.tsx
+++ client/src/components/ChartDiagram.tsx
@@ -1,3 +1,4 @@
+// ChartDiagram.tsx
 import React from "react";
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
 import { ChartDiagramProps } from "./types/Chart/interface";
@@ -6,25 +7,63 @@ import { PeriodDiv } from "./types/Chart/enum";
 const ChartDiagram: React.FC<ChartDiagramProps> = ({ chartDiagramData, handlePeriodDivChange }) => {
 	const getMaxCloseValue = () => {
 		const maxClose = Math.max(...chartDiagramData.map((data: any) => data.close));
-		console.log(chartDiagramData.length);
 		return Math.ceil(maxClose / 10000) * 10000;
+	};
+
+	const [selectedPeriod, setSelectedPeriod] = React.useState<PeriodDiv>(PeriodDiv.DAILY);
+
+	const handleButtonClick = (period: PeriodDiv) => {
+		setSelectedPeriod(period);
+		handlePeriodDivChange(period);
 	};
 
 	return (
 		<div className="w-full">
-			<div>
-				<button className="bg-gray-200 p-2 rounded-md mx-2" onClick={() => handlePeriodDivChange(PeriodDiv.DAILY)}>
-					Day
-				</button>
-				<button className="bg-gray-200 p-2 rounded-md mx-2" onClick={() => handlePeriodDivChange(PeriodDiv.WEEKLY)}>
-					Week
-				</button>
-				<button className="bg-gray-200 p-2 rounded-md mx-2" onClick={() => handlePeriodDivChange(PeriodDiv.MONTHLY)}>
-					Month
-				</button>
-				<button className="bg-gray-200 p-2 rounded-md mx-2" onClick={() => handlePeriodDivChange(PeriodDiv.YEARLY)}>
-					Max
-				</button>
+			<div className="flex justify-start my-4">
+				<div className="relative mx-2">
+					<button
+						className={`px-4 py-2 rounded-md hover:bg-gray-200 ${
+							selectedPeriod === PeriodDiv.DAILY ? "text-green-600" : "text-gray-700"
+						}`}
+						onClick={() => handleButtonClick(PeriodDiv.DAILY)}
+					>
+						3달
+					</button>
+					{selectedPeriod === PeriodDiv.DAILY && <div className="absolute bottom-0 left-0 w-full h-1 bg-green-600" />}
+				</div>
+				<div className="relative mx-2">
+					<button
+						className={`px-4 py-2 rounded-md hover:bg-gray-200 ${
+							selectedPeriod === PeriodDiv.WEEKLY ? "text-green-600" : "text-gray-700"
+						}`}
+						onClick={() => handleButtonClick(PeriodDiv.WEEKLY)}
+					>
+						2년
+					</button>
+					{selectedPeriod === PeriodDiv.WEEKLY && <div className="absolute bottom-0 left-0 w-full h-1 bg-green-600" />}
+				</div>
+				<div className="relative mx-2">
+					<button
+						className={`px-4 py-2 rounded-md hover:bg-gray-200 ${
+							selectedPeriod === PeriodDiv.MONTHLY ? "text-green-600" : "text-gray-700"
+						}`}
+						onClick={() => handleButtonClick(PeriodDiv.MONTHLY)}
+					>
+						5년
+					</button>
+					{selectedPeriod === PeriodDiv.MONTHLY && <div className="absolute bottom-0 left-0 w-full h-1 bg-green-600" />}
+				</div>
+				<div className="relative mx-2">
+					<button
+						className={`px-4 py-2 rounded-md hover:bg-gray-200 ${
+							selectedPeriod === PeriodDiv.YEARLY ? "text-green-600" : "text-gray-700"
+						}`}
+						onClick={() => handleButtonClick(PeriodDiv.YEARLY)}
+					>
+						최대
+					</button>
+					{selectedPeriod === PeriodDiv.YEARLY && <div className="absolute bottom-0 left-0 w-full h-1 bg-green-600" />}
+				</div>
 			</div>
 			<ResponsiveContainer width="95%" height={window.innerHeight / 4}>
 				<AreaChart
@@ -40,8 +79,8 @@ const ChartDiagram: React.FC<ChartDiagramProps> = ({ chartDiagramData, handlePer
 				>
 					<defs>
 						<linearGradient id="color" x1="0" y1="0" x2="0" y2="1">
-							<stop offset="5%" stopColor="#FF0000" stopOpacity={0.8} />
-							<stop offset="95%" stopColor="#FF0000" stopOpacity={0.2} />
+							<stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+							<stop offset="95%" stopColor="#82ca9d" stopOpacity={0.2} />
 						</linearGradient>
 					</defs>
 					<CartesianGrid strokeDasharray="10 0" vertical={false} />
@@ -49,21 +88,20 @@ const ChartDiagram: React.FC<ChartDiagramProps> = ({ chartDiagramData, handlePer
 						dataKey="name"
 						axisLine={false}
 						tickLine={true}
-						tick={{ stroke: "black", strokeWidth: 0.5 }}
+						// tick={{ stroke: "black", strokeWidth: 0.5 }}
 						minTickGap={Math.floor(chartDiagramData.length) * 3}
-						// angle={-45}
 					/>
 					<YAxis
 						type="number"
 						domain={["auto", getMaxCloseValue() + 1000]}
-						tick={{ stroke: "black", strokeWidth: 0.5 }}
+						// tick={{ stroke: "black", strokeWidth: 0.5 }}
 						dataKey="close"
 						axisLine={false}
 					/>
 					<Tooltip />
 					<Area type="monotone" dataKey="date" stroke="none" activeDot={false} fillOpacity={0} fill="#808080" />
 					<Area type="monotone" dataKey="open" stroke="none" activeDot={false} fillOpacity={0} fill="#808080" />
-					<Area type="monotone" dataKey="close" stroke="#8884d8" fillOpacity={1} fill="url(#color)" />
+					<Area type="monotone" dataKey="close" stroke="#82ca9d" fillOpacity={1} fill="url(#color)" />
 					<Area type="monotone" dataKey="high" stroke="none" activeDot={false} fillOpacity={0} fill="#808080" />
 					<Area type="monotone" dataKey="low" stroke="none" activeDot={false} fillOpacity={0} fill="#808080" />
 					<Area type="monotone" dataKey="volume" stroke="none" activeDot={false} fillOpacity={0} fill="#808080" />
