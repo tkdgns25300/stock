765189a0ad1b9061bb0164b264a73da44bef3bfe
sanghun lee <tkdgns25300@gmail.com>
2023-08-03 13:43:23 +0900

    feat: (apiserver) BalanceSheet Entity 생성
    대차대조표용 엔티티를 생성하였습니다.

---
+++ server/src/entities/BalanceSheet.entity.ts
@@ -0,0 +1,63 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("balanceSheet")
+export class BalanceSheet {
+	@PrimaryGeneratedColumn({
+		comment: "대차대조표 아이디",
+	})
+	id: number;
+
+	@Column({
+		type: "datetime",
+		comment: "결산 년월",
+	})
+	stac_yymm: Date;
+
+	@Column({
+		type: "bigint",
+		comment: "유동자산",
+	})
+	cras: number;
+
+	@Column({
+		type: "bigint",
+		comment: "고정자산",
+	})
+	fxas: number;
+
+	@Column({
+		type: "bigint",
+		comment: "자산총계",
+	})
+	total_aset: number;
+
+	@Column({
+		type: "bigint",
+		comment: "유동부채",
+	})
+	flow_lblt: number;
+
+	@Column({
+		type: "bigint",
+		comment: "고정부채",
+	})
+	fix_lblt: number;
+
+	@Column({
+		type: "bigint",
+		comment: "부채총계",
+	})
+	total_lblt: number;
+
+	@Column({
+		type: "bigint",
+		comment: "자본금",
+	})
+	cpfn: number;
+
+	@Column({
+		type: "bigint",
+		comment: "자본총계",
+	})
+	total_cptl: number;
+}
