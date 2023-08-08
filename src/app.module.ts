5599d0a7be0196e52c52eb517dea84b25869e7da
이상훈 <tkdgns25300@gmail.com>
2023-08-08 09:00:00 +0900

    set bond module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { StockModule } from './stock/stock.module';
 import { LoggingMiddleware } from './middleware/logging.middleware';
 import { IndexModule } from './index/index.module';
 import { EtfModule } from './etf/etf.module';
+import { BondModule } from './bond/bond.module';
 
 @Module({
-  imports: [StockModule, IndexModule, EtfModule],
+  imports: [StockModule, IndexModule, EtfModule, BondModule],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
