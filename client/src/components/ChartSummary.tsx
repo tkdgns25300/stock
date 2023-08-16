397ec6cce28f312e55a1fb5bec3dd322bbf41c1f
sanghun lee <tkdgns25300@gmail.com>
2023-08-16 13:43:23 +0900

    fix: (client) 전일 종가 수정
    전일 종가란이 현재 시세가로 보이는 버그를 수정하였습니다.

--- client/src/components/ChartSummary.tsx
+++ client/src/components/ChartSummary.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { ChartSummaryProps } from "./types/Chart/interface";
 
 const ChartSummary: React.FC<ChartSummaryProps> = ({ priceInfoData }) => {
+	console.log(priceInfoData);
 	return (
 		// <div className="p-4 border rounded-lg max-w-xs mx-auto font-doHyeon text-xl">
 		<div className="w-full p-4 border rounded-lg max-w-xs mx-auto font-gothic-ai text-sm text-gray-800">
 			<div className="mb-2 flex justify-between">
 				<span>전일 종가</span>
-				<span className="font-bold text-base">₩{priceInfoData.stckPrpr}</span>
+				<span className="font-bold text-base">₩{priceInfoData.stckPrpr - priceInfoData.prdyVrss}</span>
 			</div>
 			<div className="border-b border-gray w-full mx-auto my-4"></div>
 			<div className="mb-2 flex justify-between">
