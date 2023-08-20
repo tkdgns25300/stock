f07c0e29b55f80b486f5c8b044792eb40bdcce89
sanghun lee <tkdgns25300@gmail.com>
2023-08-20 13:43:23 +0900

    fix: (client) box shadow 설정
    box shadow가 설정이 되어있지않아 따로 설정해주었습니다.

--- client/src/components/ResultRight.tsx
+++ client/src/components/ResultRight.tsx
@@ -11,13 +11,13 @@ interface ResultRightProps {
 const ResultRight: React.FC<ResultRightProps> = ({ companyName, stockCode, stockType }) => {
 	return (
 		<div className="bg-gray-200 text-black w-1/2 h-screen flex flex-col justify-start">
-			<div className="relative mx-12 mt-24 mb-12 shadow-xl w-auto">
+			<div className="relative mx-12 mt-24 mb-12 shadow-xl w-auto" style={{ borderRadius: "2rem" }}>
 				<div className="absolute bg-green -top-14 bg-green-600 p-12 pt-4 rounded-3xl text-2xl font-doHyeon z-10">
 					Chart
 				</div>
 				<Chart stockCode={stockCode} />
 			</div>
-			<div className="relative mx-12 my-12 shadow-xl w-auto">
+			<div className="relative mx-12 my-12 shadow-xl w-auto" style={{ borderRadius: "2rem" }}>
 				<div className="absolute bg-green -top-14 bg-green-600 p-12 pt-4 rounded-3xl text-2xl font-doHyeon z-10">
 					Finance
 				</div>
