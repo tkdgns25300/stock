97b90fe3c06d882b543a2c1258845aeb16caa0a1
sanghun lee <tkdgns25300@gmail.com>
2023-08-21 13:43:23 +0900

    feat: (client) finance info data 타입설정
    재무 관련 정보를 제공하는 컴포넌트에 사용되는 interface들을
    작성하였습니다.

--- client/src/components/types/Chart/interface.tsx
+++ client/src/components/types/Chart/interface.tsx
@@ -60,3 +60,68 @@ export interface ChartRealTimePriceProps {
 export interface ChartSummaryProps {
 	priceInfoData: PriceInfoData;
 }
+
+export interface FinanceProps {
+	stockCode: string;
+}
+
+export interface IncomeStatementData {
+	stacYymm: string; // 결산년월
+	saleAccount: string; // 매출액
+	saleCost: string; // 매출원가
+	saleTotlPrfi: string; // 매출총이익
+	bsopPrti: string; // 영업이익
+	opPrfi: string; // 경상이익
+	specPrfi: string; // 특별이익
+	specLoss: string; // 특별손실
+	thtrNtin: string; // 당기순이익
+}
+
+export interface BalanceSheetData {
+	stacYymm: string; // 결산년월
+	totalAset: string; // 자산총계
+	totalLblt: string; // 부채총계
+	totalCptl: string; // 자본총계
+	cras: string; // 유동자산
+	fxas: string; // 고정자산
+	flow_lblt: string; // 유동부채
+	fix_lblt: string; // 고정부채
+	cpfn: string; // 자본금
+}
+
+export interface FinancialRatioData {
+	stacYymm: string; // 결산년월
+	grs: string; // 매출액 증가율
+	bsopPrfiInrt: string; // 영업이익 증가율
+	ntinInrt: string; // 순이익 증가율
+	roeVal: string; // ROE 값
+	eps: string; // EPS
+	sps: string; // 주당매출액
+	bps: string; // BPS
+	rsrvRate: string; // 유보비율
+	lbltRate: string; // 부채비율
+}
+
+export interface ProfitRatioData {
+	stacYymm: string; // 결산년월
+	cptlNtinRate: string; // 총자본 순이익율
+	selfCptlNtinInrt: string; // 자기자본 순이익율
+	saleNtinRate: string; // 매출액 순이익율
+	saleTotlRate: string; // 매출액 총이익율
+}
+
+export interface IncomeStatementProps {
+	IncomeStatementData: IncomeStatementData[];
+}
+
+export interface BalanceSheetProps {
+	BalanceSheetData: BalanceSheetData[];
+}
+
+export interface FinancialRatioProps {
+	FinancialRatioData: FinancialRatioData[];
+}
+
+export interface ProfitRatioProps {
+	ProfitRatioData: ProfitRatioData[];
+}
