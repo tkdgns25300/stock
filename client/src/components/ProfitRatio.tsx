f75b6e08bfd075db46389860fde3b30521fbff8d
sanghun lee <tkdgns25300@gmail.com>
2023-08-22 13:43:23 +0900

    feat: (client) ProfitRatio 컴포넌트 생성
    수익성 비율을 보여주는 ProfitRatio 컴포넌트를 생성하였습니다.

---
+++ client/src/components/ProfitRatio.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProfitRatio: React.FC = () => {
+	return (
+		<div className="w-full">
+			<div className="text-3xl font-gothic-ai">수익성비율</div>
+		</div>
+	);
+};
+
+export default ProfitRatio;
