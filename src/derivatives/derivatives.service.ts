eae5cf52b9c54a3db2bb19e65e0213b6c3040ee2
이상훈 <tkdgns25300@gmail.com>
2023-09-20 09:00:00 +0900

    주식선물 일멸매매정보 조회 API

--- src/derivatives/derivatives.service.ts
+++ src/derivatives/derivatives.service.ts
@@ -23,7 +23,7 @@ export class DerivativesService {
 
 	async eqsfuStkByddTrd() {
 		try {
-			const url = "http://data-dbg.krx.co.kr/svc/apis/drv/fut_bydd_trd?basDd=20200414";
+			const url = "http://data-dbg.krx.co.kr/svc/apis/drv/eqsfu_stk_bydd_trd?basDd=20200414";
 
 			const response = await axios.get(url, {
 				responseType: "json",
@@ -31,8 +31,66 @@ export class DerivativesService {
 					AUTH_KEY: process.env.API_KEY,
 				},
 			});
+
+			// /**
+			//  * API 요청자가 참여자일 경우 익명 게시글의 게시자(user Email) 삭제 후 리턴
+			//  * - 현재 유저의 권한먼저 확인(관리자 or 참여자)
+			//  */
+			// const spaceMember = await this.spaceMemberRepository.findOne({
+			// 	where: {
+			// 		space: { id: spaceId },
+			// 		user: { id: user.id },
+			// 	},
+			// });
+			// // 참여자가 아닐 경우
+			// if (!spaceMember) {
+			// 	return new PageResObj({}, "Not Participant", true);
+			// }
+
+			// const currentUserAuth: SpaceRoleType = spaceMember.memberRoleType;
+			// if (currentUserAuth === SpaceRoleType.PARTICIPANT) {
+			// 	for (const post of allPost) {
+			// 		if (post.isAnonymous && post.authorEmail !== user.email) {
+			// 			delete post.authorEmail;
+			// 		}
+			// 	}
+			// }
+
+			// return new PageResObj(allPost, "Get All Post Success");
+
+			return new PageResObj(response.data, "주식선물(유가) 일별매매정보 조회에 성공하였습니다.");
 		} catch (error) {
 			return new PageResObj({}, error.message, true);
 		}
 	}
 }
+
+// /**
+//  * 유저의 권한 확인
+//  * 관리자인가 소유자인가
+//  */
+// let isOwner: boolean;
+// let isAdmin: boolean = false;
+
+// const spaceMember = await this.spaceMemberRepository.findOne({
+// 	where: {
+// 		space: { id: spaceId },
+// 		user: { id: user.id },
+// 	},
+// });
+// // 참여자가 아닐 경우
+// if (!spaceMember) {
+// 	return new PageResObj({}, "Not Participant", true);
+// }
+
+// const currentUserAuth: SpaceRoleType = spaceMember.memberRoleType;
+// if (currentUserAuth === SpaceRoleType.ADMIN) isAdmin = true;
+// if (post.authorEmail === user.email) isOwner = true;
+
+// if (!isAdmin && !isOwner) {
+// 	return new PageResObj({}, "Not Authorized: Neither Admin Nor Owner", true);
+// }
+
+// // 삭제(soft-delete)
+// post.isDeleted = true;
+// await this.postRepository.save(post);
