274d0498e3ef6044a5c4c9b63f21683ac2b566cb
이상훈 <tkdgns25300@gmail.com>
2023-09-27 09:00:00 +0900

    set url and response

--- src/derivatives/derivatives.service.ts
+++ src/derivatives/derivatives.service.ts
@@ -40,6 +40,40 @@ export class DerivativesService {
 
 	async eqkfuKsqByddTrd() {
 		try {
+			const url = "http://data-dbg.krx.co.kr/svc/apis/drv/eqkfu_ksq_bydd_trd?basDd=20200414";
+
+			const response = await axios.get(url, {
+				responseType: "json",
+				headers: {
+					AUTH_KEY: process.env.API_KEY,
+				},
+			});
+
+			// /**
+			//  * API 요청자가 참여자일 경우 익명 게시글의 게시자(user Email) 삭제 후 리턴
+			//  * - 현재 유저의 권한먼저 확인(관리자 or 참여자)
+			//  */
+			// const spaceMember = await this.spaceMemberRepository.findOne({
+			// 	where: {
+			// 		space: { id: spaceId },
+			// 		user: { id: user.id },
+			// 	},
+			// });
+			// // 참여자가 아닐 경우
+			// if (!spaceMember) {
+			// 	return new PageResObj({}, "Not Participant", true);
+			// }
+
+			// const currentUserAuth: SpaceRoleType = spaceMember.memberRoleType;
+			// if (currentUserAuth === SpaceRoleType.PARTICIPANT) {
+			// 	for (const post of allPost) {
+			// 		if (post.isAnonymous && post.authorEmail !== user.email) {
+			// 			delete post.authorEmail;
+			// 		}
+			// 	}
+			// }
+
+			// return new PageResObj(allPost, "Get All Post Success");
 		} catch (error) {
 			return new PageResObj({}, error.message, true);
 		}
