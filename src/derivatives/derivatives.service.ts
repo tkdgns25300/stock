2290feb9357f872d49a757bac15286bf9da6f959
이상훈 <tkdgns25300@gmail.com>
2023-09-28 09:00:00 +0900

    주식선물(코스닥) 일별매매정보 조회 API

--- src/derivatives/derivatives.service.ts
+++ src/derivatives/derivatives.service.ts
@@ -49,6 +49,8 @@ export class DerivativesService {
 				},
 			});
 
+			return new PageResObj(response.data, "주식선물(유가) 일별매매정보 조회에 성공하였습니다.");
+
 			// /**
 			//  * API 요청자가 참여자일 경우 익명 게시글의 게시자(user Email) 삭제 후 리턴
 			//  * - 현재 유저의 권한먼저 확인(관리자 or 참여자)
@@ -79,3 +81,33 @@ export class DerivativesService {
 		}
 	}
 }
+
+// /**
+//  * 유저의 권한 확인
+//  * 관리자인가 소유자인가
+//  */
+// let isOwner: boolean;
+// let isAdmin: boolean = false;
+
+// const spaceMember = await this.spaceMemberRepository.findOne({
+// 	where: {
+// 		space: { id: spaceId },
+// 		user: { id: user.id },
+// 	},
+// });
+// // 참여자가 아닐 경우
+// if (!spaceMember) {
+// 	return new PageResObj({}, "Not Participant", true);
+// }
+
+// const currentUserAuth: SpaceRoleType = spaceMember.memberRoleType;
+// if (currentUserAuth === SpaceRoleType.ADMIN) isAdmin = true;
+// if (post.authorEmail === user.email) isOwner = true;
+
+// if (!isAdmin && !isOwner) {
+// 	return new PageResObj({}, "Not Authorized: Neither Admin Nor Owner", true);
+// }
+
+// // 삭제(soft-delete)
+// post.isDeleted = true;
+// await this.postRepository.save(post);
