0fff46e1b0f4c607c5644ae8d43b740350ab956f
sanghun lee <tkdgns25300@gmail.com>
2023-10-02 13:43:23 +0900

    feat: (client) NewsItem 에 출판일 수정
    오늘로부터 언제 출판한 뉴스기사인지 보여주는 로직 추가

--- client/src/components/NewsItem.tsx
+++ client/src/components/NewsItem.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { formatDistanceToNow, parseISO } from "date-fns";
+import { ko } from "date-fns/locale";
 import { NewsItemProps } from "./types/News/interface";
 
-const NewsItem: React.FC<NewsItemProps> = ({ title, link, pubDate, content, contentSnippet, guid, isoDate }) => {
+// 날짜를 올바른 형식으로 변환하는 함수
+const parseDate = (dateString: string) => {
+	const parsedDate = Date.parse(dateString);
+	if (!isNaN(parsedDate)) {
+		return new Date(parsedDate).toISOString();
+	}
+	// 파싱 실패 시 원래 문자열 반환 (필요에 따라 예외 처리 가능)
+	return dateString;
+};
+
+const NewsItem: React.FC<NewsItemProps> = ({ title, link, pubDate, contentSnippet }) => {
+	const relativePubDate = formatDistanceToNow(parseISO(parseDate(pubDate)), { addSuffix: true, locale: ko });
+
 	return (
 		<li className="mb-4 p-4 bg-gray-100 rounded-md">
 			<h3 className="font-bold text-lg mb-2">
@@ -10,7 +24,7 @@ const NewsItem: React.FC<NewsItemProps> = ({ title, link, pubDate, content, cont
 				</a>
 			</h3>
 			<p className="text-gray-700 mb-2">{contentSnippet}</p>
-			<p className="text-gray-500 text-sm">Published Date: {pubDate}</p>
+			<p className="text-gray-500 text-sm">{relativePubDate}</p>
 			<div className="border-b border-gray-300 w-full mx-auto my-4"></div>
 		</li>
 	);
