b7943dbedc50e1e559c25773e11a56ed93f524b4
sanghun lee <tkdgns25300@gmail.com>
2023-10-08 13:43:23 +0900

    feat: (client) investment opinion 타입 생성
    증권사별 투자 의견 데이터의 타입 생성하였습니다.

---
+++ client/src/components/types/InvestmentOpinion/interface.tsx
@@ -0,0 +1,19 @@
+export interface InvestmentOpinionProps {
+	stockCode: string;
+}
+
+export interface NewsData {
+	stckBsopDate: string;
+	invtOpnn: string;
+	invtOpnnClsCode: string;
+	mbcrName: string;
+	htsGoalPrc: string;
+}
+
+export interface NewsItemProps {
+	stckBsopDate: string;
+	invtOpnn: string;
+	invtOpnnClsCode: string;
+	mbcrName: string;
+	htsGoalPrc: string;
+}
