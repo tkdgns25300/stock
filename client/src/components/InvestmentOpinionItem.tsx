1b3d1880f0b97586e630a3992f30d250adfa8ce4
sanghun lee <tkdgns25300@gmail.com>
2023-10-12 13:43:23 +0900

    feat: (client) InvestmentOpinionItem 컴포넌트 생성
    종목 투자 의견의 개별 의견을 나타내는 컴포넌트 생성하였습니다.

---
+++ client/src/components/InvestmentOpinionItem.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { InvestmentOpinionItemProps } from "./types/InvestmentOpinion/interface";
+
+const InvestmentOpinionItem: React.FC<InvestmentOpinionItemProps> = ({
+	stckBsopDate,
+	invtOpnn,
+	invtOpnnClsCode,
+	mbcrName,
+	htsGoalPrc,
+}) => {
+	return <div>ok</div>;
+};
+
+export default InvestmentOpinionItem;
--- client/src/components/types/InvestmentOpinion/interface.tsx
+++ client/src/components/types/InvestmentOpinion/interface.tsx
@@ -3,11 +3,11 @@ export interface InvestmentOpinionProps {
 }
 
 export interface InvestmentOpinionData {
-	stckBsopDate: string;
-	invtOpnn: string;
-	invtOpnnClsCode: string;
-	mbcrName: string;
-	htsGoalPrc: string;
+	stckBsopDate: string; // 발표일
+	invtOpnn: string; // 투자 의견
+	invtOpnnClsCode: string; // 투자 의견 구분 코드
+	mbcrName: string; // 증권사명
+	htsGoalPrc: string; // 목표가
 }
 
 export interface InvestmentOpinionItemProps {
