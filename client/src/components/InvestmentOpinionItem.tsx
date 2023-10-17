92561295bd25d8d60c47c993ffa4e1a7afe61de0
sanghun lee <tkdgns25300@gmail.com>
2023-10-17 13:43:23 +0900

    improve: (client) 종목 투자 의견에 '괴리율' 추가
    서버로부터 받아온 현재가와 목표가를 이용해 '괴리율' 란을 표에
    추가하였습니다.

--- client/src/components/InvestmentOpinionItem.tsx
+++ client/src/components/InvestmentOpinionItem.tsx
@@ -8,12 +8,20 @@ const InvestmentOpinionItem: React.FC<InvestmentOpinionItemProps> = ({
 	mbcrName,
 	htsGoalPrc,
 }) => {
+	const htsGoalPrcNum = parseFloat(htsGoalPrc.replace(/,/g, ""));
+	const stckPrprNum = parseFloat(stckPrpr.replace(/,/g, ""));
+	const disparityRate = ((htsGoalPrcNum - stckPrprNum) / stckPrprNum) * 100;
+	const isHigher = htsGoalPrcNum > stckPrprNum;
+	const isLower = htsGoalPrcNum < stckPrprNum;
+
+	const textColorClass = isHigher ? "text-red-500" : isLower ? "text-blue-500" : "text-gray-500";
+
 	return (
 		<tr>
 			<td className="px-4 py-2 border border-gray-300">{stckBsopDate}</td>
-			<td className="px-4 py-2 border border-gray-300">{invtOpnn}</td>
-			<td className="px-4 py-2 border border-gray-300">{stckPrpr}</td>
-			<td className="px-4 py-2 border border-gray-300">{htsGoalPrc.toLocaleString()}</td>
+			<td className={`px-4 py-2 border border-gray-300 ${textColorClass}`}>{invtOpnn}</td>
+			<td className={`px-4 py-2 border border-gray-300 ${textColorClass}`}>{htsGoalPrcNum.toLocaleString()}</td>
+			<td className={`px-4 py-2 border border-gray-300 ${textColorClass}`}>{disparityRate.toFixed(2)}%</td>
 			<td className="px-4 py-2 border border-gray-300">{mbcrName}</td>
 		</tr>
 	);
