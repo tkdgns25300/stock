4a2ae0173aac24b588a24925f9ef744d6c499bfb
sanghun lee <tkdgns25300@gmail.com>
2023-10-30 13:43:23 +0900

    test: (client) github action test코드 작성

--- client/src/components/HomeRight.tsx
+++ client/src/components/HomeRight.tsx
@@ -6,7 +6,7 @@ const HomeRight: React.FC = () => {
 		<div className="bg-white text-black w-1/2 h-screen flex flex-col justify-center items-center">
 			<InfoMessage
 				title="기업 정보를 손쉽게 조회하기 위한 웹사이트입니다."
-				description="기업명 혹은 종목코드로 검색해보세요.test"
+				description="기업명 혹은 종목코드로 검색해보세요.testtest"
 			/>
 		</div>
 	);
