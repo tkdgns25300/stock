a04f922238e7080348282771366a9ed358b01221
sanghun lee <tkdgns25300@gmail.com>
2023-11-03 13:43:23 +0900

    test: (client) cloudfront 캐시 무효화 테스트를 위한 테스트 코드 작성

--- client/src/components/HomeRight.tsx
+++ client/src/components/HomeRight.tsx
@@ -5,8 +5,8 @@ const HomeRight: React.FC = () => {
 	return (
 		<div className="bg-white text-black w-1/2 h-screen flex flex-col justify-center items-center">
 			<InfoMessage
-				title="기업 정보를 손쉽게 조회하기 위한 웹사이트입니다."
-				description="기업명 혹은 종목코드로 검색해보세요."
+				title="기업 정보를 손쉽게 조회하기 위한 웹사이트입니다.asdfasdf"
+				description="기업명 혹은 종목코드로 검색해보세요.asdfsadf"
 			/>
 		</div>
 	);
