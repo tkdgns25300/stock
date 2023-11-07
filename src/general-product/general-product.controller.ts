1d9f730f4b76a8ef0de8be3692b38fe60e4b1002
이상훈 <tkdgns25300@gmail.com>
2023-11-07 09:00:00 +0900

    석유시장 일별매매정보 조회 API 세팅

--- src/general-product/general-product.controller.ts
+++ src/general-product/general-product.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from "@nestjs/common";
+import { GeneralProductService } from "./general-product.service";
 
-@Controller('general-product')
-export class GeneralProductController {}
+@Controller("general-product")
+export class GeneralProductController {
+	constructor(private generalProductService: GeneralProductService) {}
+
+	// 석유시장 일별매매정보
+	@Get("oil_bydd_trd")
+	getFutByddTrd() {
+		return this.generalProductService.futByddTrd();
+	}
+}
--- src/general-product/general-product.service.ts
+++ src/general-product/general-product.service.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { PageResObj } from "src/api/response/page-res-obj";
+import axios from "axios";
 
 @Injectable()
-export class GeneralProductService {}
+export class GeneralProductService {
+	async futByddTrd() {
+		try {
+		} catch (error) {
+			return new PageResObj({}, error.message, true);
+		}
+	}
+}
