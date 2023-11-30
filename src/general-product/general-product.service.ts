3f643f09ea3fb10b2caee473458c4e2d68cd0225
이상훈 <tkdgns25300@gmail.com>
2023-11-30 09:00:00 +0900

    delete unusable code

--- src/general-product/general-product.service.ts
+++ src/general-product/general-product.service.ts
@@ -81,33 +81,3 @@ export class GeneralProductService {
 		}
 	}
 }
-
-// /**
-//  * 유저의 권한 확인
-//  * 관리자인가 소유자인가
-//  */
-// let isOwner: boolean;
-// let isAdmin: boolean = false;
-
-// const spaceMember = await this.spaceMemberRepository.findOne({
-// 	where: {
-// 		space: { id: spaceId },
-// 		user: { id: user.id },
-// 	},
-// });
-// // 참여자가 아닐 경우
-// if (!spaceMember) {
-// 	return new PageResObj({}, "Not Participant", true);
-// }
-
-// const currentUserAuth: SpaceRoleType = spaceMember.memberRoleType;
-// if (currentUserAuth === SpaceRoleType.ADMIN) isAdmin = true;
-// if (post.authorEmail === user.email) isOwner = true;
-
-// if (!isAdmin && !isOwner) {
-// 	return new PageResObj({}, "Not Authorized: Neither Admin Nor Owner", true);
-// }
-
-// // 삭제(soft-delete)
-// post.isDeleted = true;
-// await this.postRepository.save(post);
