de9b6bf4470e88468e0ea21469956f6a2073dbd1
이상훈 <tkdgns25300@gmail.com>
2023-12-07 09:00:00 +0900

    사회책임투자채권 정보 조회 API 세팅

--- src/esg/esg.controller.ts
+++ src/esg/esg.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from "@nestjs/common";
+import { EsgService } from "./esg.service";
 
-@Controller('esg')
-export class EsgController {}
+@Controller("esg")
+export class EsgController {
+	constructor(private esgService: EsgService) {}
+
+	// 사회책임투자채권 정보
+	@Get("sri_bond_info")
+	getSriBondInfo() {
+		return this.esgService.sriBondInfo();
+	}
+}
