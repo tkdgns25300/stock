d6f2a0c49fb55eabd55f31214f9ce64e98d597b5
sanghun lee <tkdgns25300@gmail.com>
2023-12-09 13:43:23 +0900

    feat: (server) update database API 작업 완료

--- server/src/modules/utils/utils.service.ts
+++ server/src/modules/utils/utils.service.ts
@@ -855,15 +855,6 @@ export class UtilsService {
 			const secondJsonData = JSON.parse(companyStockJsonData);
 			const curDatabaseStockInfo = await this.stockInfoRepository.find();
 
-			// const thirdJsonData = secondJsonData
-			// 	.map((companyInfo) => {
-			// 		const newStockInfo = companyInfo.stock_info.filter(
-			// 			(e) => !curDatabaseStockInfo.some((stockInfo) => e.stock_code === stockInfo.stock_code),
-			// 		);
-			// 		if (newStockInfo.length === 0) return null;
-			// 		return { ...companyInfo, stock_info: newStockInfo };
-			// 	})
-			// 	.filter((companyInfo) => companyInfo !== null);
 			const thirdJsonData = secondJsonData.filter((companyInfo) => {
 				return companyInfo.stock_info.every((e) => {
 					return !curDatabaseStockInfo.some((stockInfo) => {
@@ -917,7 +908,54 @@ export class UtilsService {
 			await fs.promises.writeFile(`${jsonFilePath}/third.json`, JSON.stringify(finalJsonData));
 			console.log("세번째 JSON 파일이 생성되었습니다.");
 
-			// 4. DB에 저장
+			/**
+			 * 5. DB에 저장
+			 */
+			for (const companyInfoData of finalJsonData) {
+				await this.companyInfoRepository.delete({ detailed_name: companyInfoData.detailed_name });
+
+				// Company Info 작성
+				const companyInfo: CompanyInfo = new CompanyInfo();
+				companyInfo.name = companyInfoData.name;
+				companyInfo.detailed_name = companyInfoData.detailed_name;
+				companyInfo.english_name = companyInfoData.english_name;
+				companyInfo.description = companyInfoData.description || null;
+				companyInfo.industry_name = companyInfoData.industry_name;
+				companyInfo.industry_code = companyInfoData.industry_code;
+				companyInfo.capital = Number(companyInfoData.capital);
+				companyInfo.currency = companyInfoData.currency;
+				companyInfo.fiscal_month = Number(companyInfoData.fiscal_month);
+				companyInfo.ceo = companyInfoData.ceo;
+				companyInfo.main_phone = companyInfoData.main_phone;
+				companyInfo.address = companyInfoData.address;
+				companyInfo.website = companyInfoData.website || null;
+				companyInfo.founded_date = new Date(companyInfoData.founded_date) || null;
+				companyInfo.stock_infos = companyInfoData.stock_info;
+
+				const stockInfoDataArr = [];
+				for (const stockInfoData of companyInfoData.stock_info) {
+					await this.stockInfoRepository.delete({ standard_code: stockInfoData.standard_code });
+
+					// Stock Info 작성
+					const stockInfo: StockInfo = new StockInfo();
+					stockInfo.standard_code = stockInfoData.standard_code;
+					stockInfo.stock_code = stockInfoData.stock_code;
+					stockInfo.listing_date = new Date(stockInfoData.listing_date);
+					stockInfo.face_value = stockInfoData.face_value;
+					stockInfo.listed_shares = Number(stockInfoData.listed_shares);
+					stockInfo.market_type = stockInfoData.market_type;
+					stockInfo.stock_type = stockInfoData.stock_type;
+					stockInfo.affiliation = stockInfoData.affiliation || null;
+					stockInfo.security_type = stockInfoData.security_type;
+					stockInfo.company_info = companyInfo;
+
+					await this.stockInfoRepository.save(stockInfo);
+					stockInfoDataArr.push(stockInfo);
+				}
+
+				companyInfo.stock_infos = stockInfoDataArr;
+				await this.companyInfoRepository.save(companyInfo);
+			}
 
 			return new ApiResponse(null, "Successfully Update Database");
 		} catch (error) {
@@ -925,7 +963,3 @@ export class UtilsService {
 		}
 	}
 }
-
-// 1. 현재 DB에 없는 회사(종목) 조회
-// 2. 해당 회사(종목) 데이터 가져오기
-// 3. DB에 저장
