bae087c221b31b877903183830c568ed8c4a33f4
이상훈 <tkdgns25300@gmail.com>
2023-12-25 09:00:00 +0900

    create user repo(not complete)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,7 @@
-import { Controller } from '@nestjs/common';
+import { Controller } from "@nestjs/common";
+import { UserService } from "./user.service";
 
-@Controller('user')
-export class UserController {}
+@Controller("user")
+export class UserController {
+	constructor(private userService: UserService) {}
+}
--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -7,4 +7,13 @@ import { ConflictException, InternalServerErrorException } from "@nestjs/common"
 import { PageResObj } from "src/api/response/page-res-obj";
 
 @CustomRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+	async createUser(createUserDto: CreateUserDto): Promise<PageResObj<{}>> {
+		const { email, password, firstName, lastName, profilePicture } = createUserDto;
+
+		const salt = await bcrypt.genSalt();
+		const hashedPassword = await bcrypt.hash(password, salt);
+
+		const user = this.create({ email, password: hashedPassword, firstName, lastName, profilePicture });
+	}
+}
