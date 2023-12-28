4ee9982f80381c79e9b7f92ce2590faab6d824de
이상훈 <tkdgns25300@gmail.com>
2023-12-28 09:00:00 +0900

    Jwt Strategy

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -7,4 +7,32 @@ import { User } from "src/user/entity/user.entity";
 import { UserRepository } from "src/user/user.repository";
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {}
+export class JwtStrategy extends PassportStrategy(Strategy) {
+	constructor(
+		@InjectRepository(UserRepository)
+		private userRepository: UserRepository,
+	) {
+		super({
+			secretOrKey: process.env.JWT_SECRET_KEY,
+			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+		});
+	}
+
+	async validate(payload): Promise<User> {
+		const { email, isAccessToken } = payload;
+		if (!isAccessToken) {
+			throw new UnauthorizedException();
+		}
+		const user: User = await this.userRepository.findOne({
+			where: {
+				email,
+			},
+		});
+
+		if (!user) {
+			throw new UnauthorizedException();
+		}
+
+		return user;
+	}
+}
