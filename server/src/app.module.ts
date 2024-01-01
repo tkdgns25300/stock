2d857b65fe2d45a45e47a028c81879ba2bfcd27c
sanghun lee <tkdgns25300@gmail.com>
2024-01-01 13:43:23 +0900

    feat: (server) redis 세팅

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,9 +5,24 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { typeOrmModuleOptions } from "./db/configs/typeorm.config";
 import { DataSource } from "typeorm";
 import { HealthModule } from "./modules/health/health.module";
+import { RedisModule } from "@liaoliaots/nestjs-redis";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 @Module({
-	imports: [TypeOrmModule.forRoot(typeOrmModuleOptions), CompanyModule, UtilsModule, HealthModule],
+	imports: [
+		TypeOrmModule.forRoot(typeOrmModuleOptions),
+		CompanyModule,
+		UtilsModule,
+		HealthModule,
+		RedisModule.forRoot({
+			config: {
+				host: process.env.REDIS_ENDPOINT,
+				port: 6379,
+			},
+		}),
+	],
 	controllers: [],
 	providers: [],
 })
