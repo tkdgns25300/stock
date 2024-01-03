c5976c5df30767a8e39e12b79c6f140a6b35afab
이상훈 <tkdgns25300@gmail.com>
2024-01-03 09:00:00 +0900

    sign in api

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,6 +5,7 @@ import { PageResObj } from "src/api/response/page-res-obj";
 import { CreateUserDto } from "src/user/dto/create-user.dto";
 import { UserRepository } from "src/user/user.repository";
 import { AuthCredentialsDto } from "./dto/auth-credential.dto";
+import * as bcrypt from "bcryptjs";
 
 @Injectable()
 export class AuthService {
@@ -22,6 +23,23 @@ export class AuthService {
 		authCredentialsDto: AuthCredentialsDto,
 	): Promise<PageResObj<{ accessToken: string; refreshToken: string }> | PageResObj<{}>> {
 		try {
+			const { email, password } = authCredentialsDto;
+			const user = await this.userRepository.findOne({
+				where: {
+					email,
+				},
+			});
+			if (user && (await bcrypt.compare(password, user.password))) {
+				// Access Token & Refresh Token 생성
+				const AccessTokenPayload = { email, isAccessToken: true };
+				const RefreshTokenPayload = { email, isAccessToken: false };
+				const accessToken = this.jwtService.sign(AccessTokenPayload, { expiresIn: "1h" }); // Access Token 유효시간 1시간
+				const refreshToken = this.jwtService.sign(RefreshTokenPayload, { expiresIn: "30d" }); // Refresh Token 유효시간 30일
+
+				return new PageResObj({ accessToken, refreshToken }, "Login Success");
+			} else {
+				return new PageResObj({}, "Login Failed", true);
+			}
 		} catch (error) {
 			return new PageResObj({}, error.message, true);
 		}
