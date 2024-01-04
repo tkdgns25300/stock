3c4a4096bb63a0d77f32b49b7c3ee2ccb7ee79c7
sanghun lee <tkdgns25300@gmail.com>
2024-01-04 13:43:23 +0900

    feat: (server) elastiCache 연동

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,24 +5,9 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { typeOrmModuleOptions } from "./db/configs/typeorm.config";
 import { DataSource } from "typeorm";
 import { HealthModule } from "./modules/health/health.module";
-import { RedisModule } from "@liaoliaots/nestjs-redis";
-import * as dotenv from "dotenv";
-
-dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 @Module({
-	imports: [
-		TypeOrmModule.forRoot(typeOrmModuleOptions),
-		CompanyModule,
-		UtilsModule,
-		HealthModule,
-		// RedisModule.forRoot({
-		// 	config: {
-		// 		host: process.env.REDIS_ENDPOINT,
-		// 		port: 6379,
-		// 	},
-		// }),
-	],
+	imports: [TypeOrmModule.forRoot(typeOrmModuleOptions), CompanyModule, UtilsModule, HealthModule],
 	controllers: [],
 	providers: [],
 })
--- server/src/modules/company/company.module.ts
+++ server/src/modules/company/company.module.ts
@@ -9,10 +9,18 @@ import { IncomeStatement } from "src/entities/IncomeStatement.entity";
 import { ProfitRatio } from "src/entities/ProfitRatio.entity";
 import { FinancialRatio } from "src/entities/FinancialRatio.entity";
 import { CacheModule } from "@nestjs/cache-manager";
+import * as redisStore from "cache-manager-ioredis";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 @Module({
 	imports: [
-		CacheModule.register(),
+		CacheModule.register({
+			store: redisStore,
+			host: process.env.REDIS_ENDPOINT,
+			port: 6379,
+		}),
 		TypeOrmModule.forFeature([CompanyInfo, StockInfo, BalanceSheet, IncomeStatement, FinancialRatio, ProfitRatio]),
 	],
 	controllers: [CompanyController],
