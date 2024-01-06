4e887b038b5d5b35b404f88d049f570a88a5fdc3
sanghun lee <tkdgns25300@gmail.com>
2024-01-06 13:43:23 +0900

    improve: (client) company subtitle 색상 수정

--- client/src/components/SubTitle.tsx
+++ client/src/components/SubTitle.tsx
@@ -8,9 +8,8 @@ interface SubTitleProps {
 const SubTitle: React.FC<SubTitleProps> = ({ englishName, foundedDate }) => {
 	return (
 		<div className="flex my-3 text-3xl">
-			<span className="italic text-green-800/75">{englishName}</span>
-			{/* <span className="mx-7 font-gothic-a1 font-semibold text-gray-400">/</span> */}
-			<span className="italic text-green-800/75 ml-3">({foundedDate}~)</span>
+			<span className="italic text-blue-600/75">{englishName}</span>
+			<span className="italic ml-3 text-blue-600/75">({foundedDate}~)</span>
 		</div>
 	);
 };
