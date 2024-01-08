1d5a57984878aaabf4181f261b4cdac0c219283d
sanghun lee <tkdgns25300@gmail.com>
2024-01-08 13:43:23 +0900

    improve: (client) Company Detail Table 표 형식으로 변경

--- client/src/components/CompanyDetailTable.tsx
+++ client/src/components/CompanyDetailTable.tsx
@@ -19,23 +19,27 @@ interface CompanyDetailTableProps {
 
 const CompanyDetailTable: React.FC<CompanyDetailTableProps> = ({ companyData }) => {
 	return (
-		<div className="text-white">
-			<h1 className="my-4 text-5xl">Detail</h1>
-			<p className="my-2">한글명(상세): {companyData.detailed_name}</p>
-			<p className="my-2">주소: {companyData.address}</p>
-			<p className="my-2">자본금: {companyData.capital}</p>
-			<p className="my-2">대표: {companyData.ceo}</p>
-			<p className="my-2">통화구분: {companyData.currency}</p>
-			<p className="my-2">결산월: {companyData.fiscal_month}</p>
-			<p className="my-2">업종코드: {companyData.industry_code}</p>
-			<p className="my-2">업종명: {companyData.industry_name}</p>
-			<p className="my-2">대표번호: {companyData.main_phone}</p>
-			<p className="my-2">
-				웹사이트:{" "}
-				<a href={companyData.website} target="_blank" rel="noopener noreferrer">
-					{companyData.website}
-				</a>{" "}
-			</p>
+		<div className="relative mt-24 shadow-xl w-auto text-black" style={{ borderRadius: "2rem" }}>
+			<div className="absolute bg-green -top-14 bg-green-600 p-12 pt-4 rounded-3xl text-2xl font-doHyeon z-10">
+				Detail
+			</div>
+			<div className="w-full bg-slate-100 relative rounded-3xl border-current p-6 z-20">
+				<p className="my-2">한글명(상세): {companyData.detailed_name}</p>
+				<p className="my-2">주소: {companyData.address}</p>
+				<p className="my-2">자본금: {companyData.capital}</p>
+				<p className="my-2">대표: {companyData.ceo}</p>
+				<p className="my-2">통화구분: {companyData.currency}</p>
+				<p className="my-2">결산월: {companyData.fiscal_month}</p>
+				<p className="my-2">업종코드: {companyData.industry_code}</p>
+				<p className="my-2">업종명: {companyData.industry_name}</p>
+				<p className="my-2">대표번호: {companyData.main_phone}</p>
+				<p className="my-2">
+					웹사이트:{" "}
+					<a href={companyData.website} target="_blank" rel="noopener noreferrer">
+						{companyData.website}
+					</a>{" "}
+				</p>
+			</div>
 		</div>
 	);
 };
