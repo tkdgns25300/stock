9a778abf4cd55d2dd9cf820de4d188d3ba16c426
이상훈 <tkdgns25300@gmail.com>
2024-01-10 09:00:00 +0900

    유저 프로필 조회 API 세팅(not completed)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
 import { UserService } from "./user.service";
+import { User } from "./entity/user.entity";
+import { GetUser } from "./get-user.decorator";
+import { PageResObj } from "src/api/response/page-res-obj";
 
 @Controller("user")
 export class UserController {
 	constructor(private userService: UserService) {}
+
+	// 모든 유저 프로필 조회(자기거는 이메일 O, 다른 사람거는 이메일 X))
+	@Get("/find")
+	getAllUsers(@GetUser() user: User): Promise<PageResObj<User[]> | PageResObj<{}>> {
+		return this.userService.getAllUsers(user);
+	}
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { UserRepository } from "./user.repository";
+import { User } from "./entity/user.entity";
+import { PageResObj } from "src/api/response/page-res-obj";
 
 @Injectable()
-export class UserService {}
+export class UserService {
+	constructor(private readonly userRepository: UserRepository) {}
+
+	async getAllUsers(user: User): Promise<PageResObj<User[]> | PageResObj<{}>> {
+		try {
+		} catch (error) {
+			return new PageResObj({}, error.message, true);
+		}
+	}
+}
