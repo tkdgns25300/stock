7ee8ebe6ed4ced4711055e3bdaa01fb24ac4ebce
sanghun lee <tkdgns25300@gmail.com>
2024-01-15 13:43:23 +0900

    refactor: (client) Not Found Description 문구 수정

--- client/src/components/NotFoundRight.tsx
+++ client/src/components/NotFoundRight.tsx
@@ -4,7 +4,7 @@ import InfoMessage from "./InfoMessage";
 const NotFoundRight: React.FC = () => {
 	return (
 		<div className="bg-white text-black w-1/2 h-screen flex flex-col justify-center items-center">
-			<InfoMessage title="이 페이지는 존재하지 않습니다." description="올바른 URL을 입력하였는지 확인해주세요. 확인" />
+			<InfoMessage title="이 페이지는 존재하지 않습니다." description="올바른 URL을 입력하였는지 확인해주세요." />
 		</div>
 	);
 };
