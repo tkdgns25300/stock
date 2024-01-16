c999fb144bccb4ae42ad55c88e36683034eecd5d
이상훈 <tkdgns25300@gmail.com>
2024-01-16 09:00:00 +0900

    delete unusable code

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -18,32 +18,6 @@ export class UserService {
 			});
 
 			return new PageResObj(users, "Find All User Success");
-
-			// /**
-			//  * 유저의 권한 확인
-			//  * 관리자인가 소유자인가
-			//  */
-			// let isOwner: boolean;
-			// let isAdmin: boolean = false;
-
-			// const spaceMember = await this.spaceMemberRepository.findOne({
-			// 	where: {
-			// 		space: { id: spaceId },
-			// 		user: { id: user.id },
-			// 	},
-			// });
-			// // 참여자가 아닐 경우
-			// if (!spaceMember) {
-			// 	return new PageResObj({}, "Not Participant", true);
-			// }
-
-			// const currentUserAuth: SpaceRoleType = spaceMember.memberRoleType;
-			// if (currentUserAuth === SpaceRoleType.ADMIN) isAdmin = true;
-			// if (post.authorEmail === user.email) isOwner = true;
-
-			// if (!isAdmin && !isOwner) {
-			// 	return new PageResObj({}, "Not Authorized: Neither Admin Nor Owner", true);
-			// }
 		} catch (error) {
 			return new PageResObj({}, error.message, true);
 		}
