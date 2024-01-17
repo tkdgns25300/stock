f3b69df2d208d6f02248c909b30e15eee4207110
sanghun lee <tkdgns25300@gmail.com>
2024-01-17 13:43:23 +0900

    improve: (client) InfoMessage 디자인 및 문구 수정

--- client/src/components/HomeRight.tsx
+++ client/src/components/HomeRight.tsx
@@ -5,8 +5,9 @@ const HomeRight: React.FC = () => {
 	return (
 		<div className="bg-white text-black w-1/2 h-screen flex flex-col justify-center items-center">
 			<InfoMessage
-				title="기업 정보를 손쉽게 조회하기 위한 웹사이트입니다."
-				description="기업명 혹은 종목코드로 검색해보세요."
+				title="About STOCKPEDIA."
+				description={`Stockpedia는 상장 종목에 관련된 모든 정보를 제공합니다.<br /><br />
+					최신 뉴스와 재무재표 및 차트 데이터까지, 주식 투자에 필요한 모든 데이터를 확인할 수 있습니다.`}
 			/>
 		</div>
 	);
--- client/src/components/InfoMessage.tsx
+++ client/src/components/InfoMessage.tsx
@@ -8,8 +8,8 @@ interface InfoMessageProps {
 const InfoMessage: React.FC<InfoMessageProps> = ({ title, description }) => {
 	return (
 		<div className="text-center">
-			<h1 className="text-3xl font-bold mb-4">{title}</h1>
-			<p className="text-lg">{description}</p>
+			<h1 className="text-3xl font-Rubik text-8xl font-normal italic mb-12">{title}</h1>
+			<p className="text-2xl font-Nanum-Gothic font-semibold" dangerouslySetInnerHTML={{ __html: description }} />
 		</div>
 	);
 };
