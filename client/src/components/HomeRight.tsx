4d6abe6626fa01021a9ac5b98f5ca8c1a400c73e
sanghun lee <tkdgns25300@gmail.com>
2024-01-18 13:43:23 +0900

    improve: (client) InfoMessage Description 문구 및 폰트 크기 수정

--- client/src/components/HomeRight.tsx
+++ client/src/components/HomeRight.tsx
@@ -6,8 +6,9 @@ const HomeRight: React.FC = () => {
 		<div className="bg-white text-black w-1/2 h-screen flex flex-col justify-center items-center">
 			<InfoMessage
 				title="About STOCKPEDIA."
-				description={`Stockpedia는 상장 종목에 관련된 모든 정보를 제공합니다.<br /><br />
-					최신 뉴스와 재무재표 및 차트 데이터까지, 주식 투자에 필요한 모든 데이터를 확인할 수 있습니다.`}
+				description={`Stockpedia는 한국 시장(KOSPI/KOSDAQ/NONEX)에 상장된 모든 종목에 대한 종합 정보를 제공합니다.<br /><br />
+					최신 뉴스와 재무재표 및 차트 데이터까지, 주식 투자에 필요한 모든 정보를 손쉽게 확인할 수 있습니다.<br /><br />
+					회사명이나 종목코드를 입력하여 관심 종목을 검색해 보세요.`}
 			/>
 		</div>
 	);
--- client/src/components/InfoMessage.tsx
+++ client/src/components/InfoMessage.tsx
@@ -9,7 +9,7 @@ const InfoMessage: React.FC<InfoMessageProps> = ({ title, description }) => {
 	return (
 		<div className="text-center">
 			<h1 className="text-3xl font-Rubik text-8xl font-normal italic mb-12">{title}</h1>
-			<p className="text-2xl font-Nanum-Gothic font-semibold" dangerouslySetInnerHTML={{ __html: description }} />
+			<p className="text-xl font-Nanum-Gothic font-semibold" dangerouslySetInnerHTML={{ __html: description }} />
 		</div>
 	);
 };
