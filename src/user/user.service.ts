b7dda34eca92211e9078a56b0a730a791b8fc297
이상훈 <tkdgns25300@gmail.com>
2024-01-22 09:00:00 +0900

    encrypt password

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,7 @@ import { UserRepository } from "./user.repository";
 import { User } from "./entity/user.entity";
 import { PageResObj } from "src/api/response/page-res-obj";
 import { UpdateUserDto } from "./dto/update-user.dto";
+import * as bcrypt from "bcryptjs";
 
 @Injectable()
 export class UserService {
@@ -34,6 +35,37 @@ export class UserService {
 
 	async updateMyProfile(updateUserDto: UpdateUserDto, user: User): Promise<PageResObj<User> | PageResObj<{}>> {
 		try {
+			let { password, firstName, lastName, profilePicture } = updateUserDto;
+			if (password) {
+				const salt = await bcrypt.genSalt();
+				password = await bcrypt.hash(password, salt);
+			}
+
+			// /**
+			//  * 유저의 권한 확인
+			//  * 관리자인가 소유자인가
+			//  */
+			// let isOwner: boolean;
+			// let isAdmin: boolean = false;
+
+			// const spaceMember = await this.spaceMemberRepository.findOne({
+			// 	where: {
+			// 		space: { id: spaceId },
+			// 		user: { id: user.id },
+			// 	},
+			// });
+			// // 참여자가 아닐 경우
+			// if (!spaceMember) {
+			// 	return new PageResObj({}, "Not Participant", true);
+			// }
+
+			// const currentUserAuth: SpaceRoleType = spaceMember.memberRoleType;
+			// if (currentUserAuth === SpaceRoleType.ADMIN) isAdmin = true;
+			// if (post.authorEmail === user.email) isOwner = true;
+
+			// if (!isAdmin && !isOwner) {
+			// 	return new PageResObj({}, "Not Authorized: Neither Admin Nor Owner", true);
+			// }
 		} catch (error) {
 			return new PageResObj({}, error.message, true);
 		}
