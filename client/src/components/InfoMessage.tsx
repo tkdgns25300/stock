ff66ea2d2f815e2d12bb7bf86007f410c27e77f2
sanghun lee <tkdgns25300@gmail.com>
2024-01-24 13:43:23 +0900

    feat: (client) Info Message 반응형 작업

--- client/src/components/InfoMessage.tsx
+++ client/src/components/InfoMessage.tsx
@@ -8,9 +8,11 @@ interface InfoMessageProps {
 const InfoMessage: React.FC<InfoMessageProps> = ({ title, description }) => {
 	return (
 		<div className="text-center mx-24">
-			<h1 className="xl:text-8xl desktop:text-7xl font-Rubikfont-normal italic mb-12">{title}</h1>
+			<h1 className="xl:text-8xl desktop:text-7xl mobile:text-6xl text-5xl font-Rubikfont-normal italic mb-12">
+				{title}
+			</h1>
 			<p
-				className="xl:text-xl desktop:text-lg font-Nanum-Gothic font-semibold"
+				className="xl:text-xl desktop:text-lg tablet:text-base text-sm font-Nanum-Gothic font-semibold"
 				dangerouslySetInnerHTML={{ __html: description }}
 			/>
 		</div>
