231d992a2e4fd1095439b662876fe61021d21e66
sanghun lee <tkdgns25300@gmail.com>
2024-02-01 13:43:23 +0900

    feat: (client) Title 반응형 작업

--- client/src/components/Title.tsx
+++ client/src/components/Title.tsx
@@ -8,9 +8,9 @@ interface TitleProps {
 
 const Title: React.FC<TitleProps> = ({ name, stockCode, stockType }) => {
 	return (
-		<div className="flex items-center my-3">
-			<h1 className="text-9xl">{name}</h1>
-			<div className="flex flex-col mx-5 text-3xl text-gray-400 font-gothic_a1 font-400">
+		<div className="flex items-center mt-6 mx-auto">
+			<h1 className="2xl:text-9xl xl:text-8xl mobile:text-7xl text-6xl">{name}</h1>
+			<div className="flex flex-col mx-5 xl:text-3xl desktop:text-2xl text-gray-400 font-gothic_a1 font-400">
 				<p>{stockCode}</p>
 				<p>{stockType}</p>
 			</div>
