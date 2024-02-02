d7511d629fd5269fce1ea0902e1e911229882f16
sanghun lee <tkdgns25300@gmail.com>
2024-02-02 13:43:23 +0900

    feat: (client) subtitle 반응형 작업

--- client/src/components/SubTitle.tsx
+++ client/src/components/SubTitle.tsx
@@ -7,7 +7,7 @@ interface SubTitleProps {
 
 const SubTitle: React.FC<SubTitleProps> = ({ englishName, foundedDate }) => {
 	return (
-		<div className="flex my-3 text-3xl">
+		<div className="flex my-1 2xl:text-3xl large:text-2xl">
 			<span className="italic text-green-600">{englishName}</span>
 			<span className="italic ml-3 text-green-600">({foundedDate}~)</span>
 		</div>
