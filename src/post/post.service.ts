3abeb233fefa731fea2e9b6361262c3cdd6bcb86
이상훈 <tkdgns25300@gmail.com>
2024-02-07 09:00:00 +0900

    check user is space member

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -65,6 +65,52 @@ export class PostService {
 			// 		}
 			// 	}
 			// }
+
+			// 참여자가 아닐 경우
+			if (!spaceMember) {
+				return new PageResObj({}, "Not Participant", true);
+			}
+			const currentUserAuth: SpaceRoleType = spaceMember.memberRoleType;
+
+			// 익명(isAnonymous)은 질문 게시글이어야하고 참여자만 가능
+
+			if (
+				createPostDto.isAnonymous === true &&
+				(currentUserAuth === SpaceRoleType.ADMIN ||
+					createPostDto.type === PostType.NOTIFICATION)
+			) {
+				return new PageResObj(
+					{},
+					"Anonymous only Possible When PARTICIPANT and QUESTION",
+					true,
+				);
+			}
+
+			// /**
+			//  * 유저의 권한 확인
+			//  * 관리자인가 소유자인가
+			//  */
+			// let isOwner: boolean;
+			// let isAdmin: boolean = false;
+
+			// const spaceMember = await this.spaceMemberRepository.findOne({
+			// 	where: {
+			// 		space: { id: spaceId },
+			// 		user: { id: user.id },
+			// 	},
+			// });
+			// // 참여자가 아닐 경우
+			// if (!spaceMember) {
+			// 	return new PageResObj({}, "Not Participant", true);
+			// }
+
+			// const currentUserAuth: SpaceRoleType = spaceMember.memberRoleType;
+			// if (currentUserAuth === SpaceRoleType.ADMIN) isAdmin = true;
+			// if (post.authorEmail === user.email) isOwner = true;
+
+			// if (!isAdmin && !isOwner) {
+			// 	return new PageResObj({}, "Not Authorized: Neither Admin Nor Owner", true);
+			// }
 		} catch (error) {
 			return new PageResObj({}, error.message, true);
 		}
