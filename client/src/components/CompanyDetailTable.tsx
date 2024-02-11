1a6319b7b1397cfdfb64760547b99dc5b423f0be
sanghun lee <tkdgns25300@gmail.com>
2024-02-11 13:43:23 +0900

    feat: (client) company detail table 반응형 작업

--- client/src/components/CompanyDetailTable.tsx
+++ client/src/components/CompanyDetailTable.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 interface CompanyTableData {
 	address: string;
@@ -18,16 +18,42 @@ interface CompanyDetailTableProps {
 }
 
 const CompanyDetailTable: React.FC<CompanyDetailTableProps> = ({ companyData }) => {
+	const [maxHeight, setMaxHeight] = useState("350px");
+
+	useEffect(() => {
+		const handleResize = () => {
+			if (window.innerWidth >= 3840) {
+				setMaxHeight("500px");
+			} else if (window.innerWidth >= 2560) {
+				setMaxHeight("400px");
+			} else if (window.innerWidth >= 1920) {
+				setMaxHeight("350px");
+			} else if (window.innerWidth >= 1024) {
+				setMaxHeight("250px");
+			} else {
+				setMaxHeight("400px");
+			}
+		};
+
+		handleResize();
+
+		window.addEventListener("resize", handleResize);
+
+		return () => window.removeEventListener("resize", handleResize);
+	}, []);
 	return (
-		<div className="w-full flex justify-center relative mt-24 shadow-xl text-black" style={{ borderRadius: "2rem" }}>
+		<div
+			className="2xl:w-3/4 w-full flex justify-center relative mt-20 shadow-xl text-black"
+			style={{ borderRadius: "2rem" }}
+		>
 			<div className="w-full">
-				<div className="absolute bg-green -top-14 bg-green-600 p-12 pt-4 rounded-3xl text-2xl font-doHyeon z-10">
+				<div className="2xl:text-2xl large:text-xl absolute bg-green -top-14 bg-green-600 p-12 pt-4 rounded-3xl font-doHyeon z-10">
 					Detail
 				</div>
 				<div
-					className="bg-slate-100 relative rounded-3xl border-current text-lg p-6 z-20"
+					className="2xl:text-lg large:text-base bg-slate-100 relative rounded-3xl border-current p-6 z-20"
 					style={{
-						maxHeight: "450px",
+						maxHeight: maxHeight,
 						overflowY: "auto",
 						scrollbarWidth: "thin" /* Firefox */,
 						WebkitOverflowScrolling: "touch",
@@ -38,22 +64,22 @@ const CompanyDetailTable: React.FC<CompanyDetailTableProps> = ({ companyData }) 
 						<span className="text-left">한글명(상세) :</span>
 						<span className="text-right">{companyData.detailed_name}</span>
 					</p>
-					<div className="border-b border-gray w-full mx-auto my-4"></div>
+					<div className="3xl:my-4 2xl:my-3 large:my-2 desktop:my-1 my-3 border-b border-gray w-full mx-auto"></div>
 					<p className="flex justify-between items-center my-2">
 						<span className="text-left">주소 :</span>
 						<span className="text-right">{companyData.address}</span>
 					</p>
-					<div className="border-b border-gray w-full mx-auto my-4"></div>
+					<div className="3xl:my-4 2xl:my-3 large:my-2 desktop:my-1 my-3 border-b border-gray w-full mx-auto"></div>
 					<p className="flex justify-between items-center my-2">
 						<span className="text-left">대표 :</span>
 						<span className="text-right">{companyData.ceo}</span>
 					</p>
-					<div className="border-b border-gray w-full mx-auto my-4"></div>
+					<div className="3xl:my-4 2xl:my-3 large:my-2 desktop:my-1 my-3 border-b border-gray w-full mx-auto"></div>
 					<p className="flex justify-between items-center my-2">
 						<span className="text-left">대표번호 :</span>
 						<span className="text-right">{companyData.main_phone}</span>
 					</p>
-					<div className="border-b border-gray w-full mx-auto my-4"></div>
+					<div className="3xl:my-4 2xl:my-3 large:my-2 desktop:my-1 my-3 border-b border-gray w-full mx-auto"></div>
 					<p className="flex justify-between items-center my-2">
 						<span className="text-left">웹사이트:</span>
 						<span className="text-right">
@@ -62,27 +88,27 @@ const CompanyDetailTable: React.FC<CompanyDetailTableProps> = ({ companyData }) 
 							</a>
 						</span>
 					</p>
-					<div className="border-b border-gray w-full mx-auto my-4"></div>
+					<div className="3xl:my-4 2xl:my-3 large:my-2 desktop:my-1 my-3 border-b border-gray w-full mx-auto"></div>
 					<p className="flex justify-between items-center my-2">
 						<span className="text-left">자본금 :</span>
 						<span className="text-right">{companyData.capital}</span>
 					</p>
-					<div className="border-b border-gray w-full mx-auto my-4"></div>
+					<div className="3xl:my-4 2xl:my-3 large:my-2 desktop:my-1 my-3 border-b border-gray w-full mx-auto"></div>
 					<p className="flex justify-between items-center my-2">
 						<span className="text-left">통화구분 :</span>
 						<span className="text-right">{companyData.currency}</span>
 					</p>
-					<div className="border-b border-gray w-full mx-auto my-4"></div>
+					<div className="3xl:my-4 2xl:my-3 large:my-2 desktop:my-1 my-3 border-b border-gray w-full mx-auto"></div>
 					<p className="flex justify-between items-center my-2">
 						<span className="text-left">결산월 :</span>
 						<span className="text-right">{companyData.fiscal_month}</span>
 					</p>
-					<div className="border-b border-gray w-full mx-auto my-4"></div>
+					<div className="3xl:my-4 2xl:my-3 large:my-2 desktop:my-1 my-3 border-b border-gray w-full mx-auto"></div>
 					<p className="flex justify-between items-center my-2">
 						<span className="text-left">업종코드 :</span>
 						<span className="text-right">{companyData.industry_code}</span>
 					</p>
-					<div className="border-b border-gray w-full mx-auto my-4"></div>
+					<div className="3xl:my-4 2xl:my-3 large:my-2 desktop:my-1 my-3 border-b border-gray w-full mx-auto"></div>
 					<p className="flex justify-between items-center my-2">
 						<span className="text-left">업종명 :</span>
 						<span className="text-right">{companyData.industry_name}</span>
