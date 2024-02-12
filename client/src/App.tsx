fe83029dd05ee96ec54249892f096ee5d9655c68
sanghun lee <tkdgns25300@gmail.com>
2024-02-12 13:43:23 +0900

    feat: (client) App 반응형 작업

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ResultPage from "./pages/ResultPage";
-import Logo from "./components/Logo";
 import NotFoundPage from "./pages/NotFound";
 
 const App: React.FC = () => {
 	return (
 		<Router>
 			<div>
-				<Logo />
 				<Routes>
 					<Route path="/" element={<HomePage />} />
 					<Route path="/result" element={<ResultPage />} />
