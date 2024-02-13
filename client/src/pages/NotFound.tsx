fbe2658068944b882fbd47ec9027664b707eef0a
sanghun lee <tkdgns25300@gmail.com>
2024-02-13 13:43:23 +0900

    feat: (client) Not Found Page 반응형 작업

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import NotFoundLeft from "../components/NotFoundLeft";
 import NotFoundRight from "../components/NotFoundRight";
+import Logo from "../components/Logo";
 
 const NotFoundPage: React.FC = () => {
 	return (
-		<div className="flex">
-			<NotFoundLeft />
-			<NotFoundRight />
+		<div>
+			<Logo />
+			<div className="flex">
+				<NotFoundLeft />
+				<NotFoundRight />
+			</div>
 		</div>
 	);
 };
