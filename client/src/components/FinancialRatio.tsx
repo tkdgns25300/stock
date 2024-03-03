1c69a4588718478e683e4b69d15f4070c3f9fc05
sanghun lee <tkdgns25300@gmail.com>
2024-03-03 13:43:23 +0900

    feat: (client) financial ratio 반응형 작업

--- client/src/components/FinancialRatio.tsx
+++ client/src/components/FinancialRatio.tsx
@@ -56,7 +56,7 @@ const FinancialRatio: React.FC<FinancialRatioProps> = ({ financialRatioData }) =
 
 	return (
 		<div className="w-full">
-			<div className="w-full mx-4 my-8">
+			<div className="tablet:block hidden w-full mx-4 my-8">
 				<ResponsiveContainer width="100%" height={400}>
 					<BarChart data={fiveFinancialRatioData} margin={{ top: 20, right: 30, left: 20, bottom: 50 }}>
 						<CartesianGrid strokeDasharray="10 0" vertical={false} />
