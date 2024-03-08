5cd22d0a10e2d817d84fca30708a128d82720dd3
sanghun lee <tkdgns25300@gmail.com>
2024-03-08 13:43:23 +0900

    feat: (client) chart summary 반응형 작업

--- client/src/components/ChartSummary.tsx
+++ client/src/components/ChartSummary.tsx
@@ -4,29 +4,31 @@ import { ChartSummaryProps } from "./types/Chart/interface";
 const ChartSummary: React.FC<ChartSummaryProps> = ({ priceInfoData }) => {
 	return (
 		// <div className="p-4 border rounded-lg max-w-xs mx-auto font-doHyeon text-xl">
-		<div className="w-full p-4 border rounded-lg max-w-xs mx-auto font-gothic-ai text-sm text-gray-800">
+		<div className="xl:w-auto w-full p-4 border rounded-lg max-w-xs mx-auto font-doHyeon 2xl:text-lg text-sm text-gray-800">
 			<div className="mb-2 flex justify-between">
 				<span>전일 종가</span>
-				<span className="font-bold text-base">₩{priceInfoData.stckPrpr - priceInfoData.prdyVrss}</span>
+				<span className="font-doHyeon 2xl:text-base xl:text-sm desktop:text-xs">
+					₩{priceInfoData.stckPrpr - priceInfoData.prdyVrss}
+				</span>
 			</div>
 			<div className="border-b border-gray w-full mx-auto my-4"></div>
 			<div className="mb-2 flex justify-between">
 				<span>일일 변동폭</span>
-				<span className="font-bold text-base">
+				<span className="font-doHyeon 2xl:text-base xl:text-sm desktop:text-xs">
 					₩{priceInfoData.stckLwpr} - ₩{priceInfoData.stckHgpr}
 				</span>
 			</div>
 			<div className="border-b border-gray w-full mx-auto my-4"></div>
 			<div className="mb-2 flex justify-between">
 				<span>52주 변동폭</span>
-				<span className="font-bold text-base">
+				<span className="font-doHyeon 2xl:text-base xl:text-sm desktop:text-xs">
 					₩{priceInfoData.w52Lwpr} - ₩{priceInfoData.w52Hgpr}
 				</span>
 			</div>
 			<div className="border-b border-gray w-full mx-auto my-4"></div>
 			<div className="mb-2 flex justify-between">
 				<span>시가총액(억)</span>
-				<span className="font-bold text-base">{priceInfoData.htsAvls} (KRW)</span>
+				<span className="font-doHyeon 2xl:text-base xl:text-sm desktop:text-xs">{priceInfoData.htsAvls} (KRW)</span>
 			</div>
 		</div>
 	);
