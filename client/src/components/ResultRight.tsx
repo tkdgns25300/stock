269fb5718f938ebe8c47e54926e71e73b12dd08b
이상훈 <tkdgns25300@gmail.com>
2024-03-28 13:43:23 +0900

    feat: (client) make result right page responsive

--- client/src/components/ResultRight.tsx
+++ client/src/components/ResultRight.tsx
@@ -14,25 +14,25 @@ const ResultRight: React.FC<ResultRightProps> = ({ companyName, stockCode, stock
 	return (
 		<div className="w-screen desktop:w-1/2 h-screen bg-gray-200 text-black flex flex-col justify-start overflow-y-auto">
 			<div className="relative mx-12 mt-24 mb-12 shadow-xl w-auto" style={{ borderRadius: "2rem" }}>
-				<div className="tablet:text-2xl mobile:text-xl text-lg absolute bg-green -top-14 bg-green-600 desktop:px-12 desktop:pt-4 mobile:px-8 mobile:pt-2 pb-20 rounded-3xl font-doHyeon z-10">
+				<div className="tablet:text-2xl mobile:text-xl text-lg absolute bg-green -top-14 bg-green-600 desktop:px-12 mobile:px-8 pt-4 pb-20 rounded-3xl font-doHyeon z-10">
 					Chart
 				</div>
 				<Chart stockCode={stockCode} />
 			</div>
 			<div className="relative mx-12 my-12 shadow-xl w-auto" style={{ borderRadius: "2rem" }}>
-				<div className="tablet:text-2xl mobile:text-xl text-lg absolute bg-green -top-14 bg-green-600 desktop:px-12 desktop:pt-4 mobile:px-8 mobile:pt-2 pb-20 rounded-3xl font-doHyeon z-10">
+				<div className="tablet:text-2xl mobile:text-xl text-lg absolute bg-green -top-14 bg-green-600 desktop:px-12 mobile:px-8 pt-4 pb-20 rounded-3xl font-doHyeon z-10">
 					Finance
 				</div>
 				<Finance stockCode={stockCode} />
 			</div>
 			<div className="relative mx-12 my-12 shadow-xl w-auto" style={{ borderRadius: "2rem" }}>
-				<div className="tablet:text-2xl mobile:text-xl text-lg absolute bg-green -top-14 bg-green-600 desktop:px-12 desktop:pt-4 mobile:px-8 mobile:pt-2 pb-20 rounded-3xl font-doHyeon z-10">
+				<div className="tablet:text-2xl mobile:text-xl text-lg absolute bg-green -top-14 bg-green-600 desktop:px-12 mobile:px-8 pt-4 pb-20 rounded-3xl font-doHyeon z-10">
 					News
 				</div>
 				<News companyName={companyName} />
 			</div>
 			<div className="relative mx-12 my-12 shadow-xl w-auto" style={{ borderRadius: "2rem" }}>
-				<div className="tablet:text-2xl mobile:text-xl text-lg absolute bg-green -top-14 bg-green-600 desktop:px-12 desktop:pt-4 mobile:px-8 mobile:pt-2 pb-20 rounded-3xl font-doHyeon z-10">
+				<div className="tablet:text-2xl mobile:text-xl text-lg absolute bg-green -top-14 bg-green-600 desktop:px-12 mobile:px-8 pt-4 pb-20 rounded-3xl font-doHyeon z-10">
 					Investment Opinion
 				</div>
 				<InvestmentOpinion stockCode={stockCode} />
