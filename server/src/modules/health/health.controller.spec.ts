531e956b146c5c72c72eceb3038cafbad39cd205
이상훈 <tkdgns25300@gmail.com>
2024-04-03 13:43:23 +0900

    test: (server) health controller 테스트 파일 생성

---
+++ server/src/modules/health/health.controller.spec.ts
@@ -0,0 +1,7 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { HealthController } from "./health.controller";
+import { HealthService } from "./health.service";
+import { INestApplication } from "@nestjs/common";
+import request from "supertest";
+
+describe("HealthController", () => {});
