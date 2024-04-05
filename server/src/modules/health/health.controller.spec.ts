fa6bf0059010f53da5df30bb3bd0b682df0c76f7
이상훈 <tkdgns25300@gmail.com>
2024-04-05 13:43:23 +0900

    test: (server) get health check 테스트 코드 작성

--- server/src/modules/health/health.controller.spec.ts
+++ server/src/modules/health/health.controller.spec.ts
@@ -4,4 +4,29 @@ import { HealthService } from "./health.service";
 import { INestApplication } from "@nestjs/common";
 import request from "supertest";
 
-describe("HealthController", () => {});
+describe("HealthController", () => {
+	let app: INestApplication;
+
+	beforeAll(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			controllers: [HealthController],
+			providers: [
+				{
+					provide: HealthService,
+					useValue: {}, // Mock 객체
+				},
+			],
+		}).compile();
+
+		app = module.createNestApplication();
+		await app.init();
+	});
+
+	it("/health (GET)", () => {
+		return request(app.getHttpServer()).get("/health").expect(200).expect("OK");
+	});
+
+	afterAll(async () => {
+		await app.close();
+	});
+});
