7a655a30246f197665fff67e4116af30931c4761
이상훈 <tkdgns25300@gmail.com>
2024-04-13 13:43:23 +0900

    test: (client) get stock list mock data 생성

--- server/src/modules/company/company.controller.spec.ts
+++ server/src/modules/company/company.controller.spec.ts
@@ -20,4 +20,58 @@ import { BalanceSheet } from "src/entities/BalanceSheet.entity";
 describe("CompanyController", () => {
 	let app: INestApplication;
 	let companyService: CompanyService;
+
+	beforeAll(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			controllers: [CompanyController],
+			providers: [
+				{
+					provide: CompanyService,
+					useValue: {
+						getStockList: jest.fn().mockResolvedValue({
+							success: true,
+							data: [
+								{
+									standard_code: "HK0000057197",
+									stock_code: "900110",
+									listing_date: new Date("2010-04-23T00:00:00.000Z"),
+									face_value: "무액면",
+									listed_shares: 431932050,
+									market_type: "KOSDAQ",
+									stock_type: "보통주",
+									affiliation: "외국기업(소속부없음)",
+									security_type: "외국주권",
+									company_info: {
+										id: 2020,
+									},
+								} as StockInfo,
+								{
+									standard_code: "HK0000214814",
+									stock_code: "900270",
+									listing_date: new Date("2016-08-18T00:00:00.000Z"),
+									face_value: "무액면",
+									listed_shares: 125535084,
+									market_type: "KOSDAQ",
+									stock_type: "보통주",
+									affiliation: "외국기업(소속부없음)",
+									security_type: "외국주권",
+									company_info: {
+										id: 2526,
+									},
+								} as StockInfo,
+							],
+						}),
+					},
+				},
+			],
+		}).compile();
+
+		app = module.createNestApplication();
+		await app.init();
+		companyService = module.get<CompanyService>(CompanyService);
+	});
+
+	afterAll(async () => {
+		await app.close();
+	});
 });
