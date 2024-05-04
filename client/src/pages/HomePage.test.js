1209b3827ebfb48fdd5e0013f392680a7e329a4a
이상훈 <tkdgns25300@gmail.com>
2024-05-04 13:43:23 +0900

    test: (client) home page test setup

---
+++ client/src/pages/HomePage.test.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import HomePage from "./HomePage";
+
+describe("HomePage Component", () => {});
