65744fe0c5251e412254e517c305f3fb1854774c
이상훈 <tkdgns25300@gmail.com>
2024-05-07 13:43:23 +0900

    test: (client) not found page test setup

---
+++ client/src/pages/NotFound.test.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import NotFoundPage from "./NotFound";
+
+describe("NotFoundPage Component", () => {});
