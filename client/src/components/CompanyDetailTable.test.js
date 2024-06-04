087029bdd19db6c4f871a16863ef31cc26764bd9
이상훈 <tkdgns25300@gmail.com>
2024-06-04 13:43:23 +0900

    test: (client) company detail table component 테스트 코드 작성

--- client/src/components/CompanyDetailTable.test.js
+++ client/src/components/CompanyDetailTable.test.js
@@ -16,4 +16,38 @@ describe("CompanyDetailTable Component", () => {
 		main_phone: "555-1234",
 		website: "http://www.businesscorp.com",
 	};
+
+	test("renders company data correctly", () => {
+		render(<CompanyDetailTable companyData={companyData} />);
+
+		expect(screen.getByText("한글명(상세) :")).toBeInTheDocument();
+		expect(screen.getByText(companyData.detailed_name)).toBeInTheDocument();
+
+		expect(screen.getByText("주소 :")).toBeInTheDocument();
+		expect(screen.getByText(companyData.address)).toBeInTheDocument();
+
+		expect(screen.getByText("대표 :")).toBeInTheDocument();
+		expect(screen.getByText(companyData.ceo)).toBeInTheDocument();
+
+		expect(screen.getByText("대표번호 :")).toBeInTheDocument();
+		expect(screen.getByText(companyData.main_phone)).toBeInTheDocument();
+
+		expect(screen.getByText("웹사이트:")).toBeInTheDocument();
+		expect(screen.getByText(companyData.website)).toHaveAttribute("href", companyData.website);
+
+		expect(screen.getByText("자본금 :")).toBeInTheDocument();
+		expect(screen.getByText(companyData.capital)).toBeInTheDocument();
+
+		expect(screen.getByText("통화구분 :")).toBeInTheDocument();
+		expect(screen.getByText(companyData.currency)).toBeInTheDocument();
+
+		expect(screen.getByText("결산월 :")).toBeInTheDocument();
+		expect(screen.getByText(companyData.fiscal_month.toString())).toBeInTheDocument();
+
+		expect(screen.getByText("업종코드 :")).toBeInTheDocument();
+		expect(screen.getByText(companyData.industry_code)).toBeInTheDocument();
+
+		expect(screen.getByText("업종명 :")).toBeInTheDocument();
+		expect(screen.getByText(companyData.industry_name)).toBeInTheDocument();
+	});
 });
