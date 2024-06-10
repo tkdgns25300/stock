1586264a809459ce19e4b4ab97a3841687f850dc
이상훈 <tkdgns25300@gmail.com>
2024-06-10 13:43:23 +0900

    test: (client) finance component mock data

--- client/src/components/Finance.test.js
+++ client/src/components/Finance.test.js
@@ -11,4 +11,60 @@ describe("Finance Component", () => {
 	beforeEach(() => {
 		jest.resetAllMocks();
 	});
+
+	test("renders loading state and then finance data", async () => {
+		const mockData = {
+			result: {
+				incomeStatement: [
+					{
+						stac_yymm: "2023-12",
+						sale_account: "10000",
+						sale_cost: "5000",
+						sale_totl_prfi: "5000",
+						bsop_prti: "2000",
+						op_prfi: "3000",
+						spec_prfi: "0",
+						spec_loss: "0",
+						thtr_ntin: "1000",
+					},
+				],
+				balanceSheet: [
+					{
+						stac_yymm: "2023-12",
+						total_aset: "20000",
+						total_lblt: "10000",
+						total_cptl: "10000",
+						cras: "5000",
+						fxas: "5000",
+						flow_lblt: "3000",
+						fix_lblt: "7000",
+						cpfn: "10000",
+					},
+				],
+				financialRatio: [
+					{
+						stac_yymm: "2023-12",
+						grs: "10%",
+						bsop_prfi_inrt: "5%",
+						ntin_inrt: "3%",
+						roe_val: "12%",
+						eps: "1.5",
+						sps: "2",
+						bps: "20",
+						rsrv_rate: "10%",
+						lblt_rate: "50%",
+					},
+				],
+				profitRatio: [
+					{
+						stac_yymm: "2023-12",
+						cptl_ntin_rate: "50%",
+						self_cptl_ntin_inrt: "25%",
+						sale_ntin_rate: "30%",
+						sale_totl_rate: "40%",
+					},
+				],
+			},
+		};
+	});
 });
