f74fa02e2e49e9231dc0c278ff7dacc400fa9c92
이상훈 <tkdgns25300@gmail.com>
2024-08-21 13:43:23 +0900

    test: (server) utils controller test code setup

---
+++ server/src/modules/utils/utils.controller.spec.ts
@@ -0,0 +1,9 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { UtilsController } from "./utils.controller";
+import { UtilsService } from "./utils.service";
+import { ApiResponse } from "src/dtos/ApiResponse.dto";
+
+describe("UtilsController", () => {
+	let utilsController: UtilsController;
+	let utilsService: UtilsService;
+});
