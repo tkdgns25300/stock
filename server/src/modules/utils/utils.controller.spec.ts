fd43acdab880b530267d328fd2eeb50a7ea672a9
이상훈 <tkdgns25300@gmail.com>
2024-08-22 13:43:23 +0900

    test: (server) utils controller test beforEach

--- server/src/modules/utils/utils.controller.spec.ts
+++ server/src/modules/utils/utils.controller.spec.ts
@@ -6,4 +6,34 @@ import { ApiResponse } from "src/dtos/ApiResponse.dto";
 describe("UtilsController", () => {
 	let utilsController: UtilsController;
 	let utilsService: UtilsService;
+
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			controllers: [UtilsController],
+			providers: [
+				{
+					provide: UtilsService,
+					useValue: {
+						csvToJsonFirst: jest.fn(),
+						csvToJsonSecond: jest.fn(),
+						csvToJsonThird: jest.fn(),
+						jsonToDatabase: jest.fn(),
+						financialInfoToDatabase: jest.fn(),
+						balanceSheetToDatabase: jest.fn(),
+						retryFailedBalanceSheetToDatabase: jest.fn(),
+						incomeStatementToDatabase: jest.fn(),
+						retryFailedIncomeStatementToDatabase: jest.fn(),
+						financialRatioToDatabase: jest.fn(),
+						retryFailedFinancialRatioToDatabase: jest.fn(),
+						profitRatioToDatabase: jest.fn(),
+						retryFailedProfitRatioToDatabase: jest.fn(),
+						databaseUpdate: jest.fn(),
+					},
+				},
+			],
+		}).compile();
+
+		utilsController = module.get<UtilsController>(UtilsController);
+		utilsService = module.get<UtilsService>(UtilsService);
+	});
 });
